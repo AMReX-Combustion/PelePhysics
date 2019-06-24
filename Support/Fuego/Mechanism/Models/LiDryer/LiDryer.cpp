6e851cde63abd83bc7c74be459ad31d9a0a25965
sireeveslbl <sireeves@lbl.gov>
2019-06-24 14:22:16 -0700

    Merged in Anne's changes. Removed unwanted extra ifndef around VCKWYR, added needed mechanism.h and included the interface to VCKWYR in mod_fuego.f90. Not sure if mechanism.h will be kept so no need to pickle it just yet.

--- Support/Fuego/Mechanism/Models/LiDryer/LiDryer.cpp
+++ Support/Fuego/Mechanism/Models/LiDryer/LiDryer.cpp
@@ -1928,7 +1928,6 @@ AMREX_GPU_HOST_DEVICE void CKWYR(double *  rho, double *  T, double *  y,  doubl
 }
 
 
-#ifndef AMREX_USE_CUDA
 /*Returns the molar production rate of species */
 /*Given rho, T, and mass fractions */
 void VCKWYR(int *  np, double *  rho, double *  T,
@@ -1951,7 +1950,6 @@ void VCKWYR(int *  np, double *  rho, double *  T,
         wdot[i] *= 1.0e-6;
     }
 }
-#endif
 
 
 /*Returns the molar production rate of species */
