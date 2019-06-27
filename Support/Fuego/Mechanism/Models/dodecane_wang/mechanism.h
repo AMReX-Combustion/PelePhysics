1c8973b64f84fa59eed022a65ac7f38476bbeef0
Anne Felden <Enna@lbl.gov>
2019-06-27 03:08:21 -0700

    [FUEGO] Update dodecane_wang

---
+++ Support/Fuego/Mechanism/Models/dodecane_wang/mechanism.h
@@ -0,0 +1,80 @@
+#ifndef MECHANISM_h
+#define MECHANISM_h
+#if 0
+/* Elements
+0  O
+1  H
+2  C
+3  N
+*/
+/* Species
+0  H
+1  O
+2  OH
+3  HO2
+4  H2
+5  H2O
+6  H2O2
+7  O2
+8  CH
+9  CH2
+10  CH2*
+11  CH3
+12  CH4
+13  HCO
+14  CH2O
+15  CH3O
+16  CH2OH
+17  CH3OH
+18  CO
+19  CO2
+20  C2H
+21  C2H2
+22  C2H3
+23  C2H4
+24  C2H5
+25  C2H6
+26  HCCO
+27  CH2CO
+28  CH3CO
+29  CH2CHO
+30  CH3CHO
+31  C3H3
+32  pC3H4
+33  aC3H4
+34  aC3H5
+35  CH3CCH2
+36  C3H6
+37  nC3H7
+38  iC3H7
+39  C2H3CHO
+40  C4H2
+41  iC4H3
+42  C4H4
+43  iC4H5
+44  C4H5-2
+45  C4H6
+46  C4H612
+47  C4H6-2
+48  C4H7
+49  C4H81
+50  pC4H9
+51  NC12H26
+52  C6H12
+53  C6H11
+54  C5H10
+55  N2
+*/
+#endif
+#define NUM_ELEMENTS 4
+#define NUM_SPECIES 56
+#define NUM_REACTIONS 289
+
+#define FUEL_ID 51
+#define OXY_ID 7 
+#define FUEL_NAME "C12H26"
+
+#define NUM_FIT 4
+#endif
+
+/* End of file  */
