5c165e6213e0f7f377dadb69b467c20c72532324
Anne Felden <Enna@lbl.gov>
2019-06-27 03:05:24 -0700

    [FUEGO] Update grimech12

---
+++ Support/Fuego/Mechanism/Models/grimech12/mechanism.h
@@ -0,0 +1,57 @@
+#ifndef MECHANISM_h
+#define MECHANISM_h
+#if 0
+/* Elements
+0  O
+1  H
+2  C
+3  N
+4  AR
+*/
+/* Species
+0  H2
+1  H
+2  O
+3  O2
+4  OH
+5  H2O
+6  HO2
+7  H2O2
+8  C
+9  CH
+10  CH2
+11  CH2(S)
+12  CH3
+13  CH4
+14  CO
+15  CO2
+16  HCO
+17  CH2O
+18  CH2OH
+19  CH3O
+20  CH3OH
+21  C2H
+22  C2H2
+23  C2H3
+24  C2H4
+25  C2H5
+26  C2H6
+27  HCCO
+28  CH2CO
+29  HCCOH
+30  N2
+31  AR
+*/
+#endif
+#define NUM_ELEMENTS 5
+#define NUM_SPECIES 32
+#define NUM_REACTIONS 177
+
+#define FUEL_ID 13
+#define OXY_ID 30
+#define FUEL_NAME "CH4"
+
+#define NUM_FIT 4
+#endif
+
+/* End of file  */
