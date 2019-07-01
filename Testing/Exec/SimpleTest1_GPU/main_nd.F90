845d093435b9a6e495b61ba0b0e49c9370588c62
Anne Felden <Enna@lbl.gov>
2019-07-01 15:08:43 -0700

    [TEST] Add the bath_ID stuff to the SimpleTest1_GPU as well. Now both SimpleTest1 test cases should work with drm19, LiDryer, grimech30 and 12, and dodecane_wang

--- Testing/Exec/SimpleTest1_GPU/main_nd.F90
+++ Testing/Exec/SimpleTest1_GPU/main_nd.F90
@@ -4,11 +4,11 @@ module main_module
 
   implicit none
 
-  integer ::  fuel_ID, oxy_ID
+  integer ::  fuel_ID, oxy_ID, bath_ID
 
 contains
 
-    subroutine extern_init(name,namlen,fuel_ID_in,oxy_ID_in) bind(C, name="extern_init")
+    subroutine extern_init(name,namlen,fuel_ID_in,oxy_ID_in,bath_ID_in) bind(C, name="extern_init")
 
     use, intrinsic :: iso_c_binding
     use network
@@ -21,10 +21,11 @@ contains
     real (kind=amrex_real) :: small_temp = 1.d-200
     real (kind=amrex_real) :: small_dens = 1.d-200
 
-    integer(c_int), intent(in) :: fuel_ID_in, oxy_ID_in
+    integer(c_int), intent(in) :: fuel_ID_in, oxy_ID_in, bath_ID_in
 
     fuel_ID = fuel_ID_in + 1
-    oxy_ID = oxy_ID_in + 1
+    oxy_ID  = oxy_ID_in + 1
+    bath_ID = bath_ID_in + 1
 
     ! initialize the external runtime parameters in
     ! extern_probin_module
@@ -102,7 +103,7 @@ contains
     eos_state%molefrac          = 0.d0
     eos_state%molefrac(oxy_ID)  = 0.2d0
     eos_state%molefrac(fuel_ID) = 0.1d0
-    eos_state%molefrac(nspec-1) = 1.d0 - eos_state%molefrac(fuel_ID) - eos_state%molefrac(oxy_ID)
+    eos_state%molefrac(bath_ID) = 1.d0 - eos_state%molefrac(fuel_ID) - eos_state%molefrac(oxy_ID)
 
     call eos_xty(eos_state)
     
