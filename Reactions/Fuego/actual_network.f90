fdd827485b4b958f413721ef523c20f3c98c13a8
Jon Rood <jon.rood@nrel.gov>
2019-08-06 16:03:12 -0600

    Can't use the network module in the actual_network module it appears so getting nspecies from chemistry module.

--- Reactions/Fuego/actual_network.f90
+++ Reactions/Fuego/actual_network.f90
@@ -1,8 +1,7 @@
 module actual_network
 
   use fuego_chemistry 
-  use network, only : nspecies
-  use chemistry_module, only : nreactions, chemistry_init, &
+  use chemistry_module, only : nspecies, nreactions, chemistry_init, &
        chemistry_close, chemistry_initialized, spec_names, elem_names, &
        L_spec_name, L_elem_name
 
