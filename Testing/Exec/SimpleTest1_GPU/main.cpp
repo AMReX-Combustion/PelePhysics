a491b67356a40bfb9d704787caf7ae938fdcd54b
Marc Day <MSDay@lbl.gov>
2019-08-08 22:04:13 -0700

    WIP

--- Testing/Exec/SimpleTest1_GPU/main.cpp
+++ Testing/Exec/SimpleTest1_GPU/main.cpp
@@ -18,6 +18,21 @@ std::string inputs_name = "";
 
 using namespace amrex;
 
+extern "C" {
+
+  void CUDART_CB foo (cudaStream_t stream, cudaError_t error, void* p)
+  {
+    int idx = *( (int *) p);
+    printf("Index %d\n",idx);
+  }
+
+  void CUDA_CB foo1( void*  userData )
+  {
+    int idx = *( (int *)(userData));
+    printf("Index: %d",idx);
+  }
+}
+
 template <typename L>
 void For (Box const& box, int nc, L f) noexcept
 {
@@ -25,23 +40,54 @@ void For (Box const& box, int nc, L f) noexcept
     const auto lo  = amrex::lbound(box);
     const auto len = amrex::length(box);
     auto ec = Gpu::ExecutionConfig(ncells);
-    ec.numBlocks.y = nc;
+    //ec.numBlocks.y = nc;
     amrex::launch_global<<<ec.numBlocks, ec.numThreads, ec.sharedMem, amrex::Gpu::gpuStream()>>>(
     [=] AMREX_GPU_DEVICE () noexcept {
-        for (int icell = blockDim.x*blockIdx.x+threadIdx.x, stride = blockDim.x*gridDim.x;
-             icell < ncells; icell += stride) {
-            int k =  icell /   (len.x*len.y);
-            int j = (icell - k*(len.x*len.y)) /   len.x;
-            int i = (icell - k*(len.x*len.y)) - j*len.x;
-            i += lo.x;
-            j += lo.y;
-            k += lo.z;
-            int n = blockIdx.y;
-            f(i,j,k,n);
+      for (int icell = blockDim.x*blockIdx.x+threadIdx.x, stride = blockDim.x*gridDim.x;
+           icell < ncells; icell += stride)
+      {
+        int k =  icell /   (len.x*len.y);
+        int j = (icell - k*(len.x*len.y)) /   len.x;
+        int i = (icell - k*(len.x*len.y)) - j*len.x;
+        i += lo.x;
+        j += lo.y;
+        k += lo.z;
+        for (int n = blockDim.y*blockIdx.y+threadIdx.y, ystride = blockDim.y*gridDim.y; n < nc; n += ystride) {
+          f(i,j,k,n);
         }
+      }
     });
     AMREX_GPU_ERROR_CHECK();
 }
+template <unsigned int blockSize>
+__device__ void warpReduce(volatile int *sdata, unsigned int tid) {
+  if (blockSize >= 64) sdata[tid] += sdata[tid + 32];
+  if (blockSize >= 32) sdata[tid] += sdata[tid + 16];
+  if (blockSize >= 16) sdata[tid] += sdata[tid + 8];
+  if (blockSize >=  8) sdata[tid] += sdata[tid + 4];
+  if (blockSize >=  4) sdata[tid] += sdata[tid + 2];
+  if (blockSize >=  2) sdata[tid] += sdata[tid + 1];
+}
+
+template <unsigned int blockSize>
+__global__ void reduce6(int *g_idata, int *g_odata, unsigned int n) {
+  extern __shared__ int sdata[];
+  unsigned int tid = threadIdx.x;
+  unsigned int i = blockIdx.x*(blockSize*2) + tid;
+  unsigned int gridSize = blockSize*2*gridDim.x;
+  sdata[tid] = 0;
+
+  while (i < n) { sdata[tid] += g_idata[i] + g_idata[i+blockSize]; i += gridSize; }
+  __syncthreads();
+
+  if (blockSize >= 512) { if (tid < 256) { sdata[tid] += sdata[tid + 256]; } __syncthreads(); }
+  if (blockSize >= 256) { if (tid < 128) { sdata[tid] += sdata[tid + 128]; } __syncthreads(); }
+  if (blockSize >= 128) { if (tid <  64) { sdata[tid] += sdata[tid +  64]; } __syncthreads(); }
+
+  if (tid < 32) warpReduce<blockSize>(sdata, tid);
+  if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+}
+
 
 
 int
@@ -50,6 +96,21 @@ main (int   argc,
 {
     Initialize(argc,argv);
     {
+      unsigned int len=1e6;
+      int *test = (int*) malloc(len*sizeof(int));
+      for (unsigned int i=0; i<len; ++i) {
+        test[i] = i*i;
+      }
+      int *dev_test;
+      cudaMalloc( (void**)&dev_test, len*sizeof(int) );
+      cudaMemcpy( dev_test, test, len, cudaMemcpyHostToDevice );
+      
+      dim3 grid, block;
+      grid.x = 512; grid.y = 1; block.x = 8; block.y = 16;
+      
+      int valTot;
+      reduce6<512><<<grid,block>>>(dev_test,&valTot,len);
+      Print() << "Total is " << valTot << std::endl;
 
       ParmParse pp;
     
@@ -91,8 +152,8 @@ main (int   argc,
       BoxArray ba(domain);
       ba.maxSize(max_size);
 
-      int num_spec;
-      num_spec = NUM_SPECIES;
+      int num_spec = NUM_SPECIES;
+      int num_reac = NUM_REACTIONS;
 
       DistributionMapping dm(ba);
 
@@ -101,8 +162,6 @@ main (int   argc,
       MultiFab temperature(ba,dm,1,num_grow);
       MultiFab density(ba,dm,1,num_grow);
 
-      IntVect tilesize(D_DECL(10240,8,32));
-    
       for (MFIter mfi(mass_frac,TilingIfNotGPU()); mfi.isValid(); ++mfi) {
  	const Box& box = mfi.tilebox();
 	initialize_data(ARLIM_3D(box.loVect()), ARLIM_3D(box.hiVect()),
@@ -115,15 +174,19 @@ main (int   argc,
       ParmParse ppa("amr");
       std::string pltfile("plt");  
       ppa.query("plot_file",pltfile);
-      std::string outfile = amrex::Concatenate(pltfile,0); // Need a number other than zero for reg test to pass
+      std::string outfile = amrex::Concatenate(pltfile,0);
       PlotFileFromMF(temperature,outfile);
 
       MultiFab wdots(ba,dm,num_spec,num_grow);
-    
+      wdots.setVal(0);
+      MultiFab Qmfab(ba,dm,num_reac,num_grow);
+      Qmfab.setVal(0);
+
 #ifdef _OPENMP
 #pragma omp parallel if (Gpu::notInLaunchRegion())
 #endif
       {
+        BL_PROFILE("MARC");
         FArrayBox Q;
         for (MFIter mfi(mass_frac,TilingIfNotGPU()); mfi.isValid(); ++mfi) {
           const Box& box = mfi.tilebox();
@@ -131,41 +194,50 @@ main (int   argc,
           const auto& Y    = mass_frac.array(mfi);
           const auto& rho  = density.array(mfi);
           const auto& wdot = wdots.array(mfi);
-          
+
+          int numPts = box.numPts();
+
+#if 0
           Q.resize(box,NUM_REACTIONS);
           Elixir iQ = Q.elixir();
           auto iQa = Q.array();
+#else
+          const auto& iQa = Qmfab.array(mfi);
+#endif
 
+#if 1
           int nR = NUM_REACTIONS;
-          For(box, nR, [=] AMREX_GPU_DEVICE (int i, int j, int k, int n)
-          {
-             Real Kf;
-             Real Kc;
-             Kf_reac_d(temp(i,j,k),n,&Kf);
-             Kc_reac_d(temp(i,j,k),n,&Kc);
-
-             Real Yloc[NUM_SPECIES];
-             for (int L=0; L<NUM_SPECIES; ++L) {
-               Yloc[L] = Y(i,j,k,L);
-             }
-             //Q_reac_d(temp(i,j,k),Yloc,Kf,Kc,iQa(i,j,k,n));
-          });
-          /*
-          amrex::For(box, NUM_SPECIES, [=] (int i, int j, int k, int n)
-          {
-             Real qfloc[NUM_REACTIONS];
-             Real qrloc[NUM_REACTIONS];
-             for (int L=0; L<NUM_REACTIONS; ++L) {
-               qfloc[L] = iqfa(i,j,k,L);
-               qrloc[L] = iqra(i,j,k,L);
-             }
-             comp_wdot_qfqr(qfloc,qrloc,wdot(i,j,k,n));
-          });
-          */
+          For(box, nR,
+              [=] AMREX_GPU_DEVICE (int i, int j, int k, int n)
+              {
+                //Real Yloc[NUM_SPECIES];
+                //for (int L=0; L<NUM_SPECIES; ++L) {
+                //  Yloc[L] = Y(i,j,k,L);
+                //}
+                iQa(i,j,k,n) = Q_reac1_d(rho(i,j,k),temp(i,j,k),&(Y(i,j,k,0)),numPts,n);
+                //iQa(i,j,k,n) = Q_reac_d(rho(i,j,k),temp(i,j,k),Yloc,n);
+              });
+#endif
+#if 1
+          For(box, num_spec,
+              [=] AMREX_GPU_DEVICE (int i, int j, int k, int n)
+              {
+                //Real qloc[NUM_REACTIONS];
+                //for (int L=0; L<NUM_REACTIONS; ++L) {
+                //  qloc[L] = iQa(i,j,k,L);
+                //}
+                //wdot(i,j,k,n) = W_spec_d(qloc,n);
+                wdot(i,j,k,n) = W_spec1_d(&(iQa(i,j,k,0)),numPts,n);
+              });
+#endif
+          //void* p = nullptr;
+          //cudaStreamAddCallback(Gpu::gpuStream(), foo, p, 0);
+          //int idx = mfi.index();
+          //cudaLaunchHostFunc(Gpu::gpuStream(), foo1, (void*)(&idx));
+        }
       }
-      }
 
-      outfile = amrex::Concatenate(pltfile,1); // Need a number other than zero for reg test to pass
+      outfile = amrex::Concatenate(pltfile,1);
       PlotFileFromMF(wdots,outfile);
 
       CKFINALIZE();
@@ -177,3 +249,4 @@ main (int   argc,
 
     return 0;
 }
+
