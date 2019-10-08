d11f061e79de5470a5b9b05d775ea5b00a66e018
Anne Felden <Enna@lbl.gov>
2019-10-08 12:24:56 -0700

    [FUEGO] Update LuEthylene mechanism

---
+++ Support/Fuego/Mechanism/Models/LuEthylene/mechanism.h
@@ -0,0 +1,55 @@
+#ifndef MECHANISM_h
+#define MECHANISM_h
+
+#if 0
+/* Elements
+0  O
+1  H
+2  C
+3  N
+4  AR
+*/
+#endif
+
+/* Species */
+#define H2_ID 0
+#define H_ID 1
+#define O_ID 2
+#define O2_ID 3
+#define OH_ID 4
+#define H2O_ID 5
+#define HO2_ID 6
+#define H2O2_ID 7
+#define CH_ID 8
+#define CH2_ID 9
+#define CH2D_ID 10
+#define CH3_ID 11
+#define CH4_ID 12
+#define CO_ID 13
+#define CO2_ID 14
+#define HCO_ID 15
+#define CH2O_ID 16
+#define CH3O_ID 17
+#define C2H2_ID 18
+#define H2CC_ID 19
+#define C2H3_ID 20
+#define C2H4_ID 21
+#define C2H5_ID 22
+#define C2H6_ID 23
+#define HCCO_ID 24
+#define CH2CO_ID 25
+#define CH2CHO_ID 26
+#define CH3CHO_ID 27
+#define aC3H5_ID 28
+#define C3H6_ID 29
+#define nC3H7_ID 30
+#define N2_ID 31
+
+#define NUM_ELEMENTS 5
+#define NUM_SPECIES 32
+#define NUM_REACTIONS 206
+
+#define NUM_FIT 4
+#endif
+
+/* End of file  */
