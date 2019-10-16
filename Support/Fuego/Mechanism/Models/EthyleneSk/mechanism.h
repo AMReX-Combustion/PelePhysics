ffd7a80160c9313457391abd5508c63fc4175268
Anne Felden <Enna@lbl.gov>
2019-10-16 11:41:11 -0700

    [FUEGO] Add a new skeletal ethylene mechanism. 29 species, 172 reactions. Derived from detailed mech of Narayanaswamy and co. (https://krithikasivaram.github.io/)

---
+++ Support/Fuego/Mechanism/Models/EthyleneSk/mechanism.h
@@ -0,0 +1,51 @@
+#ifndef MECHANISM_h
+#define MECHANISM_h
+
+#if 0
+/* Elements
+0  N
+1  H
+2  O
+3  C
+*/
+#endif
+
+/* Species */
+#define N2_ID 0
+#define H_ID 1
+#define H2_ID 2
+#define O_ID 3
+#define OH_ID 4
+#define O2_ID 5
+#define H2O2_ID 6
+#define H2O_ID 7
+#define HO2_ID 8
+#define CO_ID 9
+#define CH3_ID 10
+#define CH2O_ID 11
+#define CO2_ID 12
+#define CH4_ID 13
+#define C2H2_ID 14
+#define C2H4_ID 15
+#define CH2CO_ID 16
+#define C2H6_ID 17
+#define C_ID 18
+#define CH_ID 19
+#define HCO_ID 20
+#define TXCH2_ID 21
+#define SXCH2_ID 22
+#define C2H3_ID 23
+#define C2H5_ID 24
+#define HCCO_ID 25
+#define CH3CHO_ID 26
+#define CH2CHO_ID 27
+#define C2H5O_ID 28
+
+#define NUM_ELEMENTS 4
+#define NUM_SPECIES 29
+#define NUM_REACTIONS 172
+
+#define NUM_FIT 4
+#endif
+
+/* End of file  */
