caba6046bc1ffcfbfd5ae8648e3cc7fabaeb1435
Anne Felden <Enna@lbl.gov>
2019-12-16 11:43:55 -0800

    [clean_jac_fuego] Add ARKODE option. If USE_ARKODE_PP=TRUE then CVODE not even build, ARKODE available only Probably not best implementation but as we are working through the kinks it should be enough

---
+++ Reactions/Fuego/CPU/actual_CARKODE.h
@@ -0,0 +1,68 @@
+#include <math.h>
+#include <iostream>
+#include <cstring>
+#include <chrono>
+
+#include <arkode/arkode_arkstep.h>
+
+#include <nvector/nvector_serial.h>    /* access to serial N_Vector            */
+#include <sunmatrix/sunmatrix_dense.h> /* access to dense SUNMatrix            */
+#include <sunlinsol/sunlinsol_dense.h> /* access to dense SUNLinearSolver      */
+#include <sunnonlinsol/sunnonlinsol_newton.h>
+#include <sundials/sundials_types.h>   /* defs. of realtype, sunindextype      */
+#include <sundials/sundials_math.h>
+
+#include <AMReX_Print.H>
+#include <Fuego_EOS.H>
+/**********************************/
+
+typedef struct {
+      /* Checks */
+      bool reactor_arkode_initialized;
+      /* Base items */
+      int ncells;
+      int iverbose;
+      int isolve_type;
+      int ianalytical_jacobian;
+      int ireactor_type;
+} *UserData;
+
+
+/**********************************/
+/* Functions Called by the Solver */
+int cF_RHS(realtype t, N_Vector y_in, N_Vector ydot, void *user_data);
+
+int cJac(realtype tn, N_Vector y, N_Vector fy, SUNMatrix J,
+		void *user_data, N_Vector tmp1, N_Vector tmp2, N_Vector tmp3);
+
+/**********************************/
+/* Functions Called by the Program */
+extern "C"
+{
+    int reactor_init(const int* cvode_iE, const int* Ncells);
+
+    int react(realtype *rY_in, realtype *rY_src_in, 
+		realtype *rX_in, realtype *rX_src_in, 
+		realtype *dt_react, realtype *time);
+
+    void reactor_close();
+}
+
+
+/**********************************/
+/* Helper functions */
+int check_flag(void *flagvalue, const char *funcname, int opt);
+
+void PrintFinalStats(void *arkodeMem, realtype Temp);
+
+UserData AllocUserData(int iE, int num_cells);
+
+void FreeUserData(UserData data);
+
+
+/**********************************/
+/* Main Kernel fct called in solver RHS */
+void fKernelSpec(realtype *dt, realtype *yvec_d, realtype *ydot_d,
+		void *user_data);
+
+
