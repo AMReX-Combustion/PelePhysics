f4d51b8799c68f6fac7b6a978cf7eff236031ae3
Anne Felden <Enna@lbl.gov>
2020-01-06 14:00:02 -0800

    [clean_jac_fuego] ReactEvalCVODE modifs to pb

--- Testing/Exec/ReactEvalCVODE/main.cpp
+++ Testing/Exec/ReactEvalCVODE/main.cpp
@@ -128,9 +128,9 @@ main (int   argc,
     /* make domain and BoxArray */
     std::vector<int> npts(3,1);
     for (int i = 0; i < BL_SPACEDIM; ++i) {
-	npts[i] = 1;
+	npts[i] = 16;
     }
-    npts[1] = 1;
+    //npts[1] = 16;
 
     amrex::Print() << "Integrating "<<npts[0]<< "x"<<npts[1]<< "x"<<npts[2]<< "  box for: ";
         amrex::Print() << dt << " seconds";
@@ -272,7 +272,7 @@ main (int   argc,
 		                &dt_incr, &time);
 #endif
 		            dt_incr =  dt/ndt;
-			    printf("%14.6e %14.6e \n", time, tmp_vect[Ncomp]);
+			    //printf("%14.6e %14.6e \n", time, tmp_vect[Ncomp]);
 			}
 		        nc = 0;
 		        for (int l = 0; l < cvode_ncells ; ++l){
