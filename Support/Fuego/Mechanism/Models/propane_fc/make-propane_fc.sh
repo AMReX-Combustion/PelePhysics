a85e4f2937f2952eb2d67ec432d5e7c6a54ba9eb
Anne Felden <Enna@lbl.gov>
2020-02-20 00:48:34 -0800

    Update propane_fc mechanism

--- Support/Fuego/Mechanism/Models/propane_fc/make-propane_fc.sh
+++ Support/Fuego/Mechanism/Models/propane_fc/make-propane_fc.sh
@@ -1,20 +1,11 @@
 
 CHEMINP=C3H8_sk_34_FC.inp
-THERMINP=thermo.dat
+THERMINP=therm.dat
 FINALFILE=propane_fc.cpp
 
 FMC=${PELE_PHYSICS_HOME}/Support/Fuego/Pythia/products/bin/fmc.py
 HEADERDIR=${PELE_PHYSICS_HOME}/Support/Fuego/Mechanism/Models/header
 
-CHEMLK=chem.asc
-LOG=chem.log
-CHEMC=chem.c
+${FUEGO_PYTHON} ${FMC} -mechanism=${CHEMINP} -thermo=${THERMINP} -name=${FINALFILE}
 
-${FUEGO_PYTHON} ${FMC} -mechanism=${CHEMINP} -thermo=${THERMINP} -name=${CHEMC}
 echo Compiling ${FINALFILE}...
-cat ${CHEMC} \
-          ${HEADERDIR}/header.start\
-          ${HEADERDIR}/header.mec   ${CHEMINP}\
-          ${HEADERDIR}/header.therm ${THERMINP}\
-          ${HEADERDIR}/header.end > ${FINALFILE}
-rm -f ${CHEMC} ${CHEMLK} ${LOG} 
