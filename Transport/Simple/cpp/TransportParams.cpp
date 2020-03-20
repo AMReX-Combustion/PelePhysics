f06a005fa1231d7d2c39b51e335c0f0c3f7b2b47
Jon Rood <jon.rood@nrel.gov>
2020-03-19 19:51:47 -0600

    Removing unnecessary extern C brackets.

--- Transport/Simple/cpp/TransportParams.cpp
+++ Transport/Simple/cpp/TransportParams.cpp
@@ -6,7 +6,6 @@
 #include "chemistry_file.H"
 #include "TransportParams.H"
 
-extern "C" {
 void egtransetWT(amrex::Real* wt);
 void egtransetEPS(amrex::Real* eps);
 void egtransetSIG(amrex::Real* sig);
@@ -17,7 +16,6 @@ void egtransetNLIN(int* nlin);
 void egtransetCOFETA(amrex::Real* fitmu);
 void egtransetCOFLAM(amrex::Real* fitlam);
 void egtransetCOFD(amrex::Real* fitdbin);
-}
 
 namespace transport_params {
 
