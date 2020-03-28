bc4b306583dfb72123c0c11ab43831cb782f279c
Jon Rood <jon.rood@nrel.gov>
2020-03-28 14:10:08 -0600

    Add clang-format file and run clang-format on EOS C++ files.

--- Eos/Fuego/cpp/EOS.cpp
+++ Eos/Fuego/cpp/EOS.cpp
@@ -7,15 +7,9 @@ AMREX_GPU_DEVICE_MANAGED int qpassMap[NPASSIVE];
 void
 init()
 {
-  AMREX_D_PICK(upassMap[0] = UMY;
-	       qpassMap[0] = QV;
-	       upassMap[1] = UMZ;
-	       qpassMap[1] = QW;
-	       int curMapIndx = 2;,
-	       upassMap[0] = UMZ;
-	       qpassMap[0] = QW;
-	       int curMapIndx = 1;,
-	       int curMapIndx = 0;);
+  AMREX_D_PICK(upassMap[0] = UMY; qpassMap[0] = QV; upassMap[1] = UMZ;
+               qpassMap[1] = QW; int curMapIndx = 2;, upassMap[0] = UMZ;
+               qpassMap[0] = QW; int curMapIndx = 1;, int curMapIndx = 0;);
   for (int i = 0; i != NUM_ADV; ++i) {
     upassMap[curMapIndx] = i + UFA;
     qpassMap[curMapIndx] = i + QFA;
--- Eos/GammaLaw/cpp/EOS.cpp
+++ Eos/GammaLaw/cpp/EOS.cpp
@@ -11,15 +11,9 @@ init()
 {
   amrex::ParmParse pp("eos");
   pp.query("gamma", gamma);
-  AMREX_D_PICK(upassMap[0] = UMY;
-	       qpassMap[0] = QV;
-	       upassMap[1] = UMZ;
-	       qpassMap[1] = QW;
-	       int curMapIndx = 2;,
-	       upassMap[0] = UMZ;
-	       qpassMap[0] = QW;
-	       int curMapIndx = 1;,
-	       int curMapIndx = 0;);
+  AMREX_D_PICK(upassMap[0] = UMY; qpassMap[0] = QV; upassMap[1] = UMZ;
+               qpassMap[1] = QW; int curMapIndx = 2;, upassMap[0] = UMZ;
+               qpassMap[0] = QW; int curMapIndx = 1;, int curMapIndx = 0;);
   for (int i = 0; i != NUM_ADV; ++i) {
     upassMap[curMapIndx] = i + UFA;
     qpassMap[curMapIndx] = i + QFA;
@@ -34,4 +28,3 @@ init()
 }
 
 } // namespace EOS
-
