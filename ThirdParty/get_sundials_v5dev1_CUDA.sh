78a34fc990d721db13fc0a65014530761031f078
Anne Felden <Enna@lbl.gov>
2020-03-30 01:07:51 -0700

    clean the script installing sundials for GPU runs on CORI. Note that this script is made to give the broadest use of sundials within PP as possible !! So flags to install KLU related libraries are still activated !

--- ThirdParty/get_sundials_v5dev1_CUDA.sh
+++ ThirdParty/get_sundials_v5dev1_CUDA.sh
@@ -10,8 +10,6 @@ if [ ! -d sundials ]; then
   git clone https://github.com/LLNL/sundials
   cd sundials/
   git checkout develop
-  git apply ../sundials-5.0.0-dev.1+cuda_nvector_allocators.patch
-  git apply ../sundials-5.0.0-dev.1+cuda_nvector_allocators_part2.patch
 else
   cd sundials/
 fi
@@ -23,12 +21,15 @@ fi
 
 
 cd builddir/
+#note that these compil flags have been tested on CORI with a gnu compiler 
 cmake    \
    -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}    \
    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON    \
    -DCMAKE_C_COMPILER=$(which gcc)    \
    -DCMAKE_CXX_COMPILER=$(which g++)    \
    -DCUDA_HOST_COMPILER=$(which g++)   \
+   -DCMAKE_CUDA_COMPILER=$(which nvcc)   \
+   -DSUNDIALS_INDEX_SIZE=32 \
    -DEXAMPLES_INSTALL_PATH=${INSTALL_PREFIX}/examples    \
    -DCMAKE_BUILD_TYPE=Release    \
    -DCMAKE_C_FLAGS_RELEASE="-O3 -DNDEBUG"    \
