cc7c38c9e937c78c230811e574d69599d8a71919
Anne Felden <Enna@lbl.gov>
2020-04-08 19:38:09 -0700

    Update Null chemistry model

--- Support/Fuego/Mechanism/Models/Null/mechanism.cpp
+++ Support/Fuego/Mechanism/Models/Null/mechanism.cpp
@@ -1,3 +1,20 @@
 #include "chemistry_file.H"
 
 void CKSYMS(int * kname, int * plenkname) {}
+
+/* Returns R, Rc, Patm */
+void CKRP(double *  ru, double *  ruc, double *  pa)
+{
+     *ru  = 8.31451e+07; 
+     *ruc = 1.98721558317399615845; 
+     *pa  = 1.01325e+06; 
+}
+
+AMREX_GPU_HOST_DEVICE void CKINIT()
+{
+}
+
+AMREX_GPU_HOST_DEVICE void CKFINALIZE()
+{
+}
+
