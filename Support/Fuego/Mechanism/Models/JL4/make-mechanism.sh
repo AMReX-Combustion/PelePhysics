aa295577ef8f6bd8755a150094fe0c9fbf8612b3
Anne Felden <Enna@lbl.gov>
2020-05-20 17:22:38 -0700

    Update JL4 LuDME Kolla Remove EthyleneSk (has been renamed ethylene_af)

--- Support/Fuego/Mechanism/Models/JL4/make-mechanism.sh
+++ Support/Fuego/Mechanism/Models/JL4/make-mechanism.sh
@@ -1,7 +1,6 @@
-
-CHEMINP=chem.inp
+CHEMINP=mechanism.inp
 THERMINP=therm.dat
-FINALFILE=LuDME.cpp
+FINALFILE=mechanism.cpp
 
 FMC=${PELE_PHYSICS_HOME}/Support/Fuego/Pythia/products/bin/fmc.py
 HEADERDIR=${PELE_PHYSICS_HOME}/Support/Fuego/Mechanism/Models/header
@@ -9,4 +8,3 @@ HEADERDIR=${PELE_PHYSICS_HOME}/Support/Fuego/Mechanism/Models/header
 ${FUEGO_PYTHON} ${FMC} -mechanism=${CHEMINP} -thermo=${THERMINP} -name=${FINALFILE}
 
 echo Compiling ${FINALFILE}...
-
