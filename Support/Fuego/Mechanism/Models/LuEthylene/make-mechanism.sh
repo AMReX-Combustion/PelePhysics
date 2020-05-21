9f4d4df2252e580b35a41efe2c2dee106f5b932a
Anne Felden <Enna@lbl.gov>
2020-05-20 17:25:05 -0700

    Update chem-H LuEthylene generation files

--- Support/Fuego/Mechanism/Models/LuEthylene/make-mechanism.sh
+++ Support/Fuego/Mechanism/Models/LuEthylene/make-mechanism.sh
@@ -1,7 +1,6 @@
-
-CHEMINP=chem.inp
+CHEMINP=mechanism.inp
 THERMINP=therm.dat
-FINALFILE=LuEthylene.cpp
+FINALFILE=mechanism.cpp
 
 FMC=${PELE_PHYSICS_HOME}/Support/Fuego/Pythia/products/bin/fmc.py
 HEADERDIR=${PELE_PHYSICS_HOME}/Support/Fuego/Mechanism/Models/header
