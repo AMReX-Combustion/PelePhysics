9bd9379d7b0aad25dd9485ebea58b23b41404eed
Anne Felden <Enna@lbl.gov>
2020-05-20 17:02:08 -0700

    Update chem-CH4-2step generation files

--- Support/Fuego/Mechanism/Models/chem-CH4-2step/make-mechanism.sh
+++ Support/Fuego/Mechanism/Models/chem-CH4-2step/make-mechanism.sh
@@ -1,7 +1,6 @@
-
-CHEMINP=chem-CH4-2step.mec
-THERMINP=chem-CH4-2step.therm
-FINALFILE=chem-CH4-2step.cpp
+CHEMINP=mechanism.inp
+THERMINP=therm.dat
+FINALFILE=mechanism.cpp
 
 FMC=${PELE_PHYSICS_HOME}/Support/Fuego/Pythia/products/bin/fmc.py
 HEADERDIR=${PELE_PHYSICS_HOME}/Support/Fuego/Mechanism/Models/header
