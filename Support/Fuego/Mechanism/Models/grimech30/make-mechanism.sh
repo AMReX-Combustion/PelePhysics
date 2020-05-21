d858aec9037f3a0ce2e489ef4fa8006e930355a7
Anne Felden <Enna@lbl.gov>
2020-05-20 17:02:45 -0700

    Update grimech30 generation files

--- Support/Fuego/Mechanism/Models/grimech30/make-mechanism.sh
+++ Support/Fuego/Mechanism/Models/grimech30/make-mechanism.sh
@@ -1,6 +1,5 @@
-
-CHEMINP=grimech30.dat
-THERMINP=thermo30.dat
+CHEMINP=mechanism.inp
+THERMINP=therm.dat
 FINALFILE=mechanism.cpp
 
 FMC=${PELE_PHYSICS_HOME}/Support/Fuego/Pythia/products/bin/fmc.py
