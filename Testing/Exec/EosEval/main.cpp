625c3cdc8bac82e376d79c51fcff697faedbe871
Anne Felden <Enna@lbl.gov>
2020-05-27 03:13:28 -0700

    WIP on EosEval, equivalent of EosEval_FORTRAN but cpp GPU friendly

--- Testing/Exec/EosEval/main.cpp
+++ Testing/Exec/EosEval/main.cpp
@@ -7,8 +7,10 @@
 
 #include <main_F.H>
 #include <PlotFileFromMF.H>
+#include "mechanism.h"
+#include <GPU_misc.H>
 
-std::string inputs_name = "";
+#include <EOS.H>
 
 using namespace amrex;
 
@@ -20,16 +22,8 @@ main (int   argc,
     {
 
       ParmParse pp;
-    
-      std::string probin_file = "probin";
-      pp.query("probin_file",probin_file);
-      int probin_file_length = probin_file.length();
-      std::vector<int> probin_file_name(probin_file_length);
 
-      for (int i = 0; i < probin_file_length; i++)
-	probin_file_name[i] = probin_file[i];
-
-      extern_init(&(probin_file_name[0]),&probin_file_length);
+      EOS::init();
     
       std::vector<int> npts(3,1);
       for (int i = 0; i < BL_SPACEDIM; ++i) {
@@ -50,53 +44,83 @@ main (int   argc,
       BoxArray ba(domain);
       ba.maxSize(max_size);
 
-      int num_spec;
-      get_num_spec(&num_spec);
+      ParmParse ppa("amr");
+      std::string pltfile("plt");
+      ppa.query("plot_file",pltfile);
 
       DistributionMapping dm{ba};
 
       int num_grow = 0;
-      MultiFab mass_frac(ba,dm,num_spec,num_grow);
+      MultiFab mass_frac(ba,dm,NUM_SPECIES,num_grow);
       MultiFab temperature(ba,dm,1,num_grow);
+      MultiFab density(ba,dm,1,num_grow);
+      MultiFab energy(ba,dm,1,num_grow);
 
       IntVect tilesize(D_DECL(10240,8,32));
     
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
-      for (MFIter mfi(mass_frac,tilesize); mfi.isValid(); ++mfi) {
+      for (MFIter mfi(mass_frac,amrex::TilingIfNotGPU()); mfi.isValid(); ++mfi) {
 	const Box& box = mfi.tilebox();
 	initialize_data(ARLIM_3D(box.loVect()), ARLIM_3D(box.hiVect()),
 			BL_TO_FORTRAN_N_3D(mass_frac[mfi],0),
 			BL_TO_FORTRAN_N_3D(temperature[mfi],0),
+			BL_TO_FORTRAN_N_3D(density[mfi],0),
+			BL_TO_FORTRAN_N_3D(energy[mfi],0),
 			&(dx[0]), &(plo[0]), &(phi[0]));
+
+	//const Box& gbox = mfi.tilebox();
+
+	//Array4<Real> const& Y_a    = mass_frac.array(mfi);
+	//Array4<Real> const& T_a    = temperature.array(mfi);
+	//Array4<Real> const& rho_a  = density.array(mfi);
+	//Array4<Real> const& e_a    = energy.array(mfi);
+
+	//amrex::ParallelFor(gbox, 
+	//    [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+	//	initialize_data(i, j, k, 
+	//			Y_a, T_a, rho_a, e_a 
+	//			dx, plo, phi);
+	//});
       }
 
+      MultiFab VarPlt(ba,dm,NUM_SPECIES+3,num_grow);
+      MultiFab::Copy(VarPlt,mass_frac,0,0,NUM_SPECIES,num_grow);
+      MultiFab::Copy(VarPlt,temperature,0,NUM_SPECIES,1,num_grow);
+      MultiFab::Copy(VarPlt,density,0,NUM_SPECIES+1,1,num_grow);
+      MultiFab::Copy(VarPlt,energy,0,NUM_SPECIES+2,1,num_grow);
+      std::string initfile = amrex::Concatenate(pltfile,99); // Need a number other than zero for reg test to pass
+      PlotFileFromMF(VarPlt,initfile);
 
       MultiFab cp(ba,dm,1,num_grow);
-    
+
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
-      for (MFIter mfi(mass_frac,tilesize); mfi.isValid(); ++mfi) {
-	const Box& box = mfi.tilebox();
-	get_cp(ARLIM_3D(box.loVect()), ARLIM_3D(box.hiVect()),
-	       BL_TO_FORTRAN_N_3D(mass_frac[mfi],0),
-	       BL_TO_FORTRAN_N_3D(temperature[mfi],0),
-	       BL_TO_FORTRAN_N_3D(cp[mfi],0));
+      for (MFIter mfi(mass_frac,amrex::TilingIfNotGPU()); mfi.isValid(); ++mfi) {
+
+	const Box& gbox = mfi.tilebox();
+
+	Array4<Real> const& Y_a    = mass_frac.array(mfi);
+	Array4<Real> const& T_a    = temperature.array(mfi);
+	Array4<Real> const& cp_a   = cp.array(mfi);
+
+	amrex::ParallelFor(gbox, 
+	    [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+	    get_cp(i, j, k, 
+	           Y_a, T_a, cp_a);
+	});
       }
 
+      EOS::close();
 
-      ParmParse ppa("amr");
-      std::string pltfile("plt");  ppa.query("plot_file",pltfile);
       std::string outfile = amrex::Concatenate(pltfile,1); // Need a number other than zero for reg test to pass
       PlotFileFromMF(cp,outfile);
 
-      extern_close();
-
     }
 
-    Finalize();
+    amrex::Finalize();
 
     return 0;
 }
--- Testing/Exec/EosEval/main_nd.F90
+++ Testing/Exec/EosEval/main_nd.F90
@@ -8,84 +8,59 @@ module main_module
 
 contains
 
-  subroutine extern_init(name,namlen) bind(C, name="extern_init")
-
-    use eos_module
-    use transport_module
-    use fuego_chemistry
-
-    implicit none
-    integer :: namlen
-    integer :: name(namlen)
-
-    real(amrex_real) :: small_temp = 1.d-200
-    real(amrex_real) :: small_dens = 1.d-200
-
-    ! initialize the external runtime parameters in
-    ! extern_probin_module
-    call runtime_init(name,namlen)
-
-    call network_init()
-
-    call eos_init(small_temp, small_dens)
-
-    call transport_init_F()
-
-  end subroutine extern_init
-
-
-  subroutine extern_close() bind(C, name="extern_close")
-
-    use transport_module
-    implicit none
-
-    call transport_close_F()
-
-  end subroutine extern_close
-
-
-  subroutine get_num_spec(nspecies_out) bind(C, name="get_num_spec")
-
-    implicit none
-
-    integer, intent(out) :: nspecies_out
-
-    nspecies_out = NUM_SPECIES
-
-  end subroutine get_num_spec
-
-
   subroutine initialize_data( &
        lo,hi, &
        massfrac,    mf_lo, mf_hi, &
        temperature,  t_lo,  t_hi, &
+       density,      r_lo,  r_hi, &
+       energy,      e_lo,  e_hi, &
        dx, plo, phi) &
        bind(C, name="initialize_data")
 
     use amrex_constants_module, only: M_PI, HALF, ONE, TWO, ZERO
+    use eos_type_module
+    use eos_module, only: eos_rt
 
     implicit none
 
     integer         , intent(in   ) ::     lo(3),    hi(3)
     integer         , intent(in   ) ::  mf_lo(3), mf_hi(3)
     integer         , intent(in   ) ::   t_lo(3),  t_hi(3)
+    integer         , intent(in   ) ::   r_lo(3),  r_hi(3)
+    integer         , intent(in   ) ::   e_lo(3),  e_hi(3)
     real(amrex_real), intent(in   ) ::     dx(3)
     real(amrex_real), intent(in   ) ::    plo(3),   phi(3)
     real(amrex_real), intent(inout) :: massfrac(mf_lo(1):mf_hi(1),mf_lo(2):mf_hi(2),mf_lo(3):mf_hi(3),NUM_SPECIES)
     real(amrex_real), intent(inout) :: temperature(t_lo(1):t_hi(1),t_lo(2):t_hi(2),t_lo(3):t_hi(3))
+    real(amrex_real), intent(inout) :: density(r_lo(1):r_hi(1),r_lo(2):r_hi(2),r_lo(3):r_hi(3))
+    real(amrex_real), intent(inout) :: energy(e_lo(1):e_hi(1),e_lo(2):e_hi(2),e_lo(3):e_hi(3))
 
     ! local variables
     integer          :: i, j, k, n
     real(amrex_real) :: Temp_lo, Temp_hi, dTemp, P(3), L(3), x, y, z, Y_lo(NUM_SPECIES), Y_hi(NUM_SPECIES)
+    real(amrex_real) :: Rho_lo, Rho_hi, dRho
+    real(amrex_real) :: energy_lo, energy_hi, denergy
     real(amrex_real) :: sum
+
+    type (eos_t) :: eos_state
+
+    call build(eos_state)
 
     Temp_lo = 500.d0
     Temp_hi = 500.d0
     dTemp = 5.d0
 
+    Rho_lo = 1.d-2
+    Rho_hi = 1.d-2
+    dRho = 5.d-3
+
     Y_lo(:) = ZERO
     Y_lo(1) = ONE
     Y_hi(:) = ONE / NUM_SPECIES
+
+    energy_lo   = 0.0d0
+    energy_hi   = 0.0d0 
+    denergy     = 100.0d0 
     
     L(:) = phi(:) - plo(:)
     P(:) = L(:) / 4
@@ -99,6 +74,7 @@ contains
 
              temperature(i,j,k) = Temp_lo + (Temp_hi-Temp_lo)*y/L(2) + dTemp*SIN(TWO*M_PI*y/P(2))
              massfrac(i,j,k,:) = Y_lo(:) + (Y_hi(:)-Y_lo(:))*x/L(1)
+             density(i,j,k) = Rho_lo + (Rho_hi-Rho_lo)*y/L(2) + dRho*SIN(TWO*M_PI*y/P(2))
 
              sum = ZERO
              do n=1,NUM_SPECIES-1
@@ -106,48 +82,14 @@ contains
              enddo
              massfrac(i,j,k,NUM_SPECIES) = ONE - sum
 
-          end do
-       end do
-    end do
-  end subroutine initialize_data
-
-  subroutine get_cp( &
-       lo,hi, &
-       massfrac,    mf_lo, mf_hi, &
-       temperature,  t_lo,  t_hi, &
-       cp,          cp_lo, cp_hi) &
-       bind(C, name="get_cp")
-
-    use eos_type_module
-    use eos_module
-
-    implicit none
-
-    integer         , intent(in   ) ::     lo(3),    hi(3)
-    integer         , intent(in   ) ::  mf_lo(3), mf_hi(3)
-    integer         , intent(in   ) ::   t_lo(3),  t_hi(3)
-    integer         , intent(in   ) ::  cp_lo(3), cp_hi(3)
-    real(amrex_real), intent(inout) :: massfrac(mf_lo(1):mf_hi(1),mf_lo(2):mf_hi(2),mf_lo(3):mf_hi(3),NUM_SPECIES)
-    real(amrex_real), intent(inout) :: temperature(t_lo(1):t_hi(1),t_lo(2):t_hi(2),t_lo(3):t_hi(3))
-    real(amrex_real), intent(  out) :: cp(cp_lo(1):cp_hi(1),cp_lo(2):cp_hi(2),cp_lo(3):cp_hi(3))
-
-    ! local variables
-    integer          :: i, j, k
-
-    type (eos_t) :: eos_state
-
-    call build(eos_state)
-
-    do k = lo(3),hi(3)
-       do j = lo(2),hi(2)
-          do i = lo(1),hi(1)
-
+             ! compute energy
              eos_state % T        = temperature(i,j,k)
              eos_state % massfrac = massfrac(i,j,k,1:NUM_SPECIES)
+             eos_state % rho      = density(i,j,k)
 
-             call eos_cp(eos_state)
+             call eos_rt(eos_state)
 
-             cp(i,j,k) = eos_state % cp
+             energy(i,j,k) = eos_state % e +  (energy_hi-energy_lo)*y/L(2) + denergy*SIN(TWO*M_PI*y/P(2))
 
           end do
        end do
@@ -155,7 +97,6 @@ contains
 
     call destroy(eos_state)
 
-  end subroutine get_cp
-
+  end subroutine initialize_data
 
 end module main_module
