990824713ce812c1b18b453fa7f464f1c70276b1
Anne M Felden <amfelden@lbl.gov>
2020-06-19 03:17:39 -0700

    Wrap the Fuego species names as strings in EOS speciesNames Add a couple functions useful for mixture fraction computations, making sure we use a CHON formalism for atomic elems

--- Eos/GammaLaw/EOS.cpp
+++ Eos/GammaLaw/EOS.cpp
@@ -19,4 +19,26 @@ close()
   CKFINALIZE();
 }
 
+void
+speciesNames(amrex::Vector<std::string>& spn) {
+  spn.push_back("AIR");
+}
+
+void
+atomic_weightsCHON(amrex::Real atwCHON[])
+{
+  //CHON
+  for (int i = 0; i < 4; i++) {
+      atwCHON[i] = 0.0;
+  }
+}
+
+void
+element_compositionCHON(int ecompCHON[])
+{
+  for (int k = 0; k < 4; k++) {
+      ecompCHON[k] = 0;
+  }
+}
+
 } // namespace EOS
