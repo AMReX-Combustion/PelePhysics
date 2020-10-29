b0c49e23bcf3677a8fb1df9d67a46cf4660e7a76
Anne Felden <amfelden@lake.lbl.gov>
2020-10-28 20:38:59 -0700

    Add a skeletal mechanism for iso-octane - contains 143 species. Has not been thoroughly checked so you know the drill use with care

---
+++ Support/Fuego/Mechanism/Models/isooctane_lu/mechanism.h
@@ -0,0 +1,165 @@
+#ifndef MECHANISM_h
+#define MECHANISM_h
+
+#if 0
+/* Elements
+0  C
+1  H
+2  O
+3  N
+*/
+#endif
+
+/* Species */
+#define H_ID 0
+#define H2_ID 1
+#define O_ID 2
+#define O2_ID 3
+#define OH_ID 4
+#define H2O_ID 5
+#define HO2_ID 6
+#define H2O2_ID 7
+#define CO_ID 8
+#define CO2_ID 9
+#define CH2O_ID 10
+#define HCO_ID 11
+#define HO2CHO_ID 12
+#define O2CHO_ID 13
+#define OCHO_ID 14
+#define CH2OH_ID 15
+#define CH3O_ID 16
+#define CH3O2H_ID 17
+#define CH3O2_ID 18
+#define CH4_ID 19
+#define CH3_ID 20
+#define CH2_ID 21
+#define CH2S_ID 22
+#define C2H6_ID 23
+#define C2H5_ID 24
+#define C2H4_ID 25
+#define C2H3_ID 26
+#define C2H2_ID 27
+#define CH3CHO_ID 28
+#define CH3CO_ID 29
+#define CH2CO_ID 30
+#define HCCO_ID 31
+#define C2H5O_ID 32
+#define C2H5O2_ID 33
+#define C2H3O12_ID 34
+#define CH3COCH3_ID 35
+#define CH3COCH2_ID 36
+#define C2H3CHO_ID 37
+#define C2H3CO_ID 38
+#define C2H5CO_ID 39
+#define IC3H7_ID 40
+#define C3H6_ID 41
+#define C3H5A_ID 42
+#define C3H5S_ID 43
+#define C3H5T_ID 44
+#define C3H4P_ID 45
+#define C3H4A_ID 46
+#define C3H3_ID 47
+#define C3H2_ID 48
+#define C3H5O_ID 49
+#define C3H6OOH21_ID 50
+#define C3H6OOH21O2_ID 51
+#define IC3H7O2_ID 52
+#define C3KET21_ID 53
+#define CH3CHCO_ID 54
+#define SC4H9_ID 55
+#define IC4H9_ID 56
+#define TC4H9_ID 57
+#define IC4H8_ID 58
+#define IC4H7_ID 59
+#define TC4H9O2_ID 60
+#define IC4H9O2_ID 61
+#define IC4H8O2HI_ID 62
+#define TC4H9O_ID 63
+#define TC4H9O2H_ID 64
+#define IC4H7O_ID 65
+#define IC3H7CHO_ID 66
+#define TC3H6CHO_ID 67
+#define IC4H8OOHIO2_ID 68
+#define IC4KETII_ID 69
+#define IC4H7OH_ID 70
+#define IC4H6OH_ID 71
+#define IC3H5CHO_ID 72
+#define IC3H5CO_ID 73
+#define TC3H6OCHO_ID 74
+#define IC3H6CO_ID 75
+#define IC4H7OOH_ID 76
+#define TC3H6O2CHO_ID 77
+#define CH2CCH2OH_ID 78
+#define BC5H11_ID 79
+#define AC5H10_ID 80
+#define BC5H10_ID 81
+#define CC5H10_ID 82
+#define AC5H9C_ID 83
+#define CC5H9B_ID 84
+#define AC5H9OC_ID 85
+#define CC5H9OB_ID 86
+#define CH3CHCHO_ID 87
+#define BC6H12_ID 88
+#define CC6H12_ID 89
+#define C5H102_ID 90
+#define IC4H7I1_ID 91
+#define YC7H15_ID 92
+#define XC7H14_ID 93
+#define YC7H14_ID 94
+#define XC7H13Z_ID 95
+#define YC7H13Y2_ID 96
+#define YC7H13OY2_ID 97
+#define YC7H15O2_ID 98
+#define ACC6H10_ID 99
+#define ACC6H9A_ID 100
+#define ACC6H9D_ID 101
+#define NEOC5H11_ID 102
+#define NEOC5H11O2_ID 103
+#define NEOC5H10OOH_ID 104
+#define TC4H9CHO_ID 105
+#define TC4H9CO_ID 106
+#define IC8H18_ID 107
+#define AC8H17_ID 108
+#define BC8H17_ID 109
+#define CC8H17_ID 110
+#define DC8H17_ID 111
+#define IC8H16_ID 112
+#define JC8H16_ID 113
+#define AC8H17O2_ID 114
+#define BC8H17O2_ID 115
+#define CC8H17O2_ID 116
+#define DC8H17O2_ID 117
+#define CC8H17O2H_ID 118
+#define CC8H17O_ID 119
+#define AC8H16OOHA_ID 120
+#define AC8H16OOHB_ID 121
+#define AC8H16OOHC_ID 122
+#define BC8H16OOHA_ID 123
+#define BC8H16OOHD_ID 124
+#define CC8H16OOHA_ID 125
+#define DC8H16OOHC_ID 126
+#define DC8H16OOHB_ID 127
+#define IC8ETERAB_ID 128
+#define IC8ETERAC_ID 129
+#define IC8ETERBD_ID 130
+#define AC8H16OOHBO2_ID 131
+#define BC8H16OOHAO2_ID 132
+#define BC8H16OOHDO2_ID 133
+#define CC8H16OOHAO2_ID 134
+#define DC8H16OOHBO2_ID 135
+#define IC8KETAB_ID 136
+#define IC8KETBA_ID 137
+#define IC8KETBD_ID 138
+#define IC8KETDB_ID 139
+#define IC3H7COC3H6T_ID 140
+#define TC4H9COC2H4S_ID 141
+#define N2_ID 142
+
+#define NUM_ELEMENTS 4
+#define NUM_SPECIES 143
+#define NUM_REACTIONS 643
+
+#define NUM_FIT 4
+#endif
+
+/* End of file  */
