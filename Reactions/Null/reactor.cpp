ad76c12c1d04a14bd58ed6eb727358d759fea673
Lucas Esclapez <lesclapez@lbl.gov>
2021-01-11 13:51:39 -0800

    Fix Reaction_dir = Null to allow GPU runs.

--- Reactions/Null/reactor.cpp
+++ Reactions/Null/reactor.cpp
@@ -5,8 +5,6 @@ using namespace amrex;
 /**********************************/
 /* Global Variables */
   int ireactor_type;
-  int eint_rho = 1; // in/out = rhoE/rhoY
-  int enth_rho = 2; // in/out = rhoH/rhoY 
 /**********************************/
 
 /**********************************/
@@ -20,10 +18,16 @@ void SetTolFactODE(double relative_tol,double absolute_tol) {}
 void ReSetTolODE() {}
 
 /* Initialization routine, called once at the begining of the problem */
-int reactor_init(int reactor_type, int ode_ncells) { 
+int reactor_init(int reactor_type, int /*ode_ncells*/) { 
     ireactor_type = reactor_type;
     return(0); 
 }
+
+#ifdef AMREX_USE_GPU
+int reactor_info(int /*reactor_type*/, int /*ode_ncells*/) {
+    return(0); 
+}
+#endif
 
 /* Main routine for CVode integration: integrate a Box version 1*/
 int react(const Box& box,
@@ -35,12 +39,28 @@ int react(const Box& box,
           Array4<Real> const& FC_in,
           Array4<int> const& mask,
           Real &dt_react,
-          Real &time) {
+          Real &time
+#ifdef AMREX_USE_GPU
+          , const int &reactor_type
+          , cudaStream_t stream
+#endif
+          ) {
 
     int omp_thread = 0;
 #ifdef _OPENMP
     omp_thread = omp_get_thread_num(); 
 #endif
+
+    // Reactor type
+#ifdef AMREX_USE_GPU
+    int reactor_type_lcl = reactor_type;
+#else
+    int reactor_type_lcl = ireactor_type;
+#endif
+
+    // Define types
+    constexpr int eint_rho = 1; // in/out = rhoE/rhoY
+    constexpr int enth_rho = 2; // in/out = rhoH/rhoY 
 
     /* Initial time and time to reach after integration */
     Real time_init = time;
@@ -72,7 +92,7 @@ int react(const Box& box,
         Real energy_loc = renergy_loc / rho_loc;
         // Get curr estimate of T
         Real T_loc    = T_in(i,j,k,0);
-        if (ireactor_type == eint_rho){
+        if (reactor_type_lcl == eint_rho){
             EOS::EY2T(energy_loc,Y_loc,T_loc);
         } else {
             EOS::HY2T(energy_loc,Y_loc,T_loc);
@@ -108,6 +128,5 @@ int react(amrex::Real *rY_in, amrex::Real *rY_src_in,
 
 /* Free memory */
 void reactor_close() {}
-
 
 /* End of file  */
--- Reactions/Null/reactor.h
+++ Reactions/Null/reactor.h
@@ -3,11 +3,14 @@
 #include <AMReX_FArrayBox.H>
 #include <AMReX_MultiFab.H>
 #include <AMReX_iMultiFab.H>
-
 #include <EOS.H>
+
 /**********************************/
 /* Functions Called by the Program */
 int reactor_init(int cvode_iE, int Ncells);
+#ifdef AMREX_USE_GPU
+int reactor_info(int cvode_iE, int Ncells);
+#endif
 
 int react(const amrex::Box& box,
           amrex::Array4<amrex::Real> const& rY_in,
@@ -18,7 +21,13 @@ int react(const amrex::Box& box,
           amrex::Array4<amrex::Real> const& FC_in,
           amrex::Array4<int> const& mask, 
           amrex::Real &dt_react,
-          amrex::Real &time);
+          amrex::Real &time
+#ifdef AMREX_USE_GPU
+          , const int reactor_type
+          , cudaStream_t stream
+#endif
+          );
+
 
 int react(amrex::Real *rY_in, amrex::Real *rY_src_in, 
 	      amrex::Real *rX_in, amrex::Real *rX_src_in, 
