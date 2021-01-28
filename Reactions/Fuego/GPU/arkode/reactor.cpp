519b78036413b9e8d8511fdab1f8a4db68545993
Jon Rood <jon.rood@nrel.gov>
2021-01-27 17:17:10 -0700

    Sundials objects needs namespaces attached.

--- Reactions/Fuego/GPU/arkode/reactor.cpp
+++ Reactions/Fuego/GPU/arkode/reactor.cpp
@@ -230,7 +230,7 @@ int react(realtype *rY_in, realtype *rY_src_in,
     user_data->nbThreads               = 32;
 
     //y = N_VNewManaged_Cuda(neq_tot);
-    y = N_VNewWithMemHelp_Cuda(neq_tot, /*use_managed_mem=*/true, *The_SUNMemory_Helper());
+    y = N_VNewWithMemHelp_Cuda(neq_tot, /*use_managed_mem=*/true, *amrex::sundials::The_SUNMemory_Helper());
     N_VSetCudaStream_Cuda(y, &stream);
 
 
