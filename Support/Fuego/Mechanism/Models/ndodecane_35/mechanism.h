e15a378d44ca64aeb4dae47753e5d196cb71a7f1
Lucas Esclapez <lesclapez@lbl.gov>
2021-03-16 09:52:43 -0700

    Generate reaction-free Fuego files.

---
+++ Support/Fuego/Mechanism/Models/ndodecane_35/mechanism.h
@@ -0,0 +1,57 @@
+#ifndef MECHANISM_h
+#define MECHANISM_h
+
+#if 0
+/* Elements
+0  O
+1  H
+2  C
+3  N
+*/
+#endif
+
+/* Species */
+#define NC12H26_ID 0
+#define H_ID 1
+#define O_ID 2
+#define OH_ID 3
+#define HO2_ID 4
+#define H2_ID 5
+#define H2O_ID 6
+#define H2O2_ID 7
+#define O2_ID 8
+#define CH3_ID 9
+#define CH4_ID 10
+#define CH2O_ID 11
+#define CO_ID 12
+#define CO2_ID 13
+#define C2H2_ID 14
+#define C2H4_ID 15
+#define C2H6_ID 16
+#define CH2CHO_ID 17
+#define aC3H5_ID 18
+#define C3H6_ID 19
+#define C2H3CHO_ID 20
+#define C4H7_ID 21
+#define C4H81_ID 22
+#define C5H9_ID 23
+#define C5H10_ID 24
+#define C6H12_ID 25
+#define C7H14_ID 26
+#define C8H16_ID 27
+#define C9H18_ID 28
+#define PXC9H19_ID 29
+#define C10H20_ID 30
+#define C12H24_ID 31
+#define C12H25O2_ID 32
+#define OC12H23OOH_ID 33
+#define N2_ID 34
+
+#define NUM_ELEMENTS 4
+#define NUM_SPECIES 35
+#define NUM_REACTIONS 0
+
+#define NUM_FIT 4
+#endif
+
+/* End of file  */
