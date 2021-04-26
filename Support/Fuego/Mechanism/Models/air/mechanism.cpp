1f15ea13106840ebd900a062c767bcb4b01463b8
Anne Felden <amfelden@lake.lbl.gov>
2021-04-26 13:15:27 -0700

    Update air mech and remove drm19/mechanism.h

--- Support/Fuego/Mechanism/Models/air/mechanism.cpp
+++ Support/Fuego/Mechanism/Models/air/mechanism.cpp
@@ -1,8 +1,377 @@
 #ifndef MECHANISM_CPP
 #define MECHANISM_CPP
 
-#include "chemistry_file.H"
+#include "mechanism.H"
 
+/* PURE CPU stuff  */
+#ifndef AMREX_USE_GPU
+namespace thermo
+{
+    amrex::Real fwd_A[0], fwd_beta[0], fwd_Ea[0];
+    amrex::Real low_A[0], low_beta[0], low_Ea[0];
+    amrex::Real rev_A[0], rev_beta[0], rev_Ea[0];
+    amrex::Real troe_a[0],troe_Ts[0], troe_Tss[0], troe_Tsss[0];
+    amrex::Real sri_a[0], sri_b[0], sri_c[0], sri_d[0], sri_e[0];
+    amrex::Real activation_units[0], prefactor_units[0], phase_units[0];
+    int is_PD[0], troe_len[0], sri_len[0], nTB[0], *TBid[0];
+    amrex::Real *TB[0];
+};
+
+using namespace thermo;
+
+
+static amrex::Real T_save = -1;
+#ifdef _OPENMP
+#pragma omp threadprivate(T_save)
+#endif
+
+static amrex::Real k_f_save[0];
+#ifdef _OPENMP
+#pragma omp threadprivate(k_f_save)
+#endif
+
+static amrex::Real Kc_save[0];
+#ifdef _OPENMP
+#pragma omp threadprivate(Kc_save)
+#endif
+
+
+/*compute the production rate for each species pointwise on CPU */
+void productionRate_cpu(amrex::Real *  wdot, amrex::Real *  sc, amrex::Real T)
+{
+    amrex::Real tc[] = { log(T), T, T*T, T*T*T, T*T*T*T }; /*temperature cache */
+    amrex::Real invT = 1.0 / tc[1];
+
+    if (T != T_save)
+    {
+        T_save = T;
+        comp_k_f(tc,invT,k_f_save);
+        comp_Kc(tc,invT,Kc_save);
+    }
+
+    amrex::Real qdot, q_f[0], q_r[0];
+    amrex::Real sc_qss[1];
+    comp_qfqr_cpu(q_f, q_r, sc, sc_qss, tc, invT);
+
+    for (int i = 0; i < 2; ++i) {
+        wdot[i] = 0.0;
+    }
+
+    return;
+}
+
+void comp_k_f(amrex::Real *  tc, amrex::Real invT, amrex::Real *  k_f)
+{
+    for (int i=0; i<0; ++i) {
+        k_f[i] = prefactor_units[i] * fwd_A[i]
+                    * exp(fwd_beta[i] * tc[0] - activation_units[i] * fwd_Ea[i] * invT);
+    };
+    return;
+}
+
+void comp_Kc(amrex::Real *  tc, amrex::Real invT, amrex::Real *  Kc)
+{
+    /*compute the Gibbs free energy */
+    amrex::Real g_RT[2];
+    gibbs(g_RT, tc);
+
+
+    for (int i=0; i<0; ++i) {
+        Kc[i] = exp(Kc[i]);
+    };
+
+    /*reference concentration: P_atm / (RT) in inverse mol/m^3 */
+    amrex::Real refC = 101325 / 8.31446 * invT;
+    amrex::Real refCinv = 1 / refC;
+
+
+    return;
+}
+
+void comp_qfqr_cpu(amrex::Real *  qf, amrex::Real *  qr, amrex::Real *  sc, amrex::Real * sc_qss, amrex::Real *  tc, amrex::Real invT)
+{
+
+    amrex::Real T = tc[1];
+
+    /*compute the mixture concentration */
+    amrex::Real mixture = 0.0;
+    for (int i = 0; i < 2; ++i) {
+        mixture += sc[i];
+    }
+    for (int i = 0; i < 0; ++i) {
+        mixture += sc_qss[i];
+    }
+
+    amrex::Real Corr[0];
+    for (int i = 0; i < 0; ++i) {
+        Corr[i] = 1.0;
+    }
+
+    for (int i=0; i<0; i++)
+    {
+        qf[i] *= Corr[i] * k_f_save[i];
+        qr[i] *= Corr[i] * k_f_save[i] / Kc_save[i];
+    }
+
+    return;
+}
+
+
+/*compute the progress rate for each reaction */
+void progressRate(amrex::Real *  qdot, amrex::Real *  sc, amrex::Real T)
+{
+    amrex::Real tc[] = { log(T), T, T*T, T*T*T, T*T*T*T }; /*temperature cache */
+    amrex::Real invT = 1.0 / tc[1];
+
+    if (T != T_save)
+    {
+        T_save = T;
+        comp_k_f(tc,invT,k_f_save);
+        comp_Kc(tc,invT,Kc_save);
+    }
+
+
+    return;
+}
+
+
+/*Returns the rate of progress for each reaction */
+void CKQC(amrex::Real *  T, amrex::Real *  C, amrex::Real *  qdot)
+{
+    int id; /*loop counter */
+
+    /*convert to SI */
+    for (id = 0; id < 2; ++id) {
+        C[id] *= 1.0e6;
+    }
+
+    /*convert to chemkin units */
+    progressRate(qdot, C, *T);
+
+    /*convert to chemkin units */
+    for (id = 0; id < 2; ++id) {
+        C[id] *= 1.0e-6;
+    }
+
+    for (id = 0; id < 0; ++id) {
+        qdot[id] *= 1.0e-6;
+    }
+}
+
+
+/*Returns the progress rates of each reactions */
+/*Given P, T, and mass fractions */
+void CKQYP(amrex::Real *  P, amrex::Real *  T, amrex::Real *  y, amrex::Real *  qdot)
+{
+    int id; /*loop counter */
+    amrex::Real c[2]; /*temporary storage */
+    amrex::Real YOW = 0; 
+    amrex::Real PWORT; 
+    amrex::Real imw[2];
+
+    get_imw(imw);
+
+    /*Compute inverse of mean molecular wt first */
+    YOW += y[0]*imw[0]; /*O2 */
+    YOW += y[1]*imw[1]; /*N2 */
+    /*PW/RT (see Eq. 7) */
+    PWORT = (*P)/(YOW * 8.31446261815324e+07 * (*T)); 
+    /*multiply by 1e6 so c goes to SI */
+    PWORT *= 1e6; 
+    /*Now compute conversion (and go to SI) */
+    c[0] = PWORT * y[0]*imw[0]; 
+    c[1] = PWORT * y[1]*imw[1]; 
+
+    /*convert to chemkin units */
+    progressRate(qdot, c, *T);
+
+    /*convert to chemkin units */
+    for (id = 0; id < 0; ++id) {
+        qdot[id] *= 1.0e-6;
+    }
+}
+
+
+/*Returns the progress rates of each reactions */
+/*Given P, T, and mole fractions */
+void CKQXP(amrex::Real *  P, amrex::Real *  T, amrex::Real *  x, amrex::Real *  qdot)
+{
+    int id; /*loop counter */
+    amrex::Real c[2]; /*temporary storage */
+    amrex::Real PORT = 1e6 * (*P)/(8.31446261815324e+07 * (*T)); /*1e6 * P/RT so c goes to SI units */
+
+    /*Compute conversion, see Eq 10 */
+    for (id = 0; id < 2; ++id) {
+        c[id] = x[id]*PORT;
+    }
+
+    /*convert to chemkin units */
+    progressRate(qdot, c, *T);
+
+    /*convert to chemkin units */
+    for (id = 0; id < 0; ++id) {
+        qdot[id] *= 1.0e-6;
+    }
+}
+
+
+/*Returns the progress rates of each reactions */
+/*Given rho, T, and mass fractions */
+void CKQYR(amrex::Real *  rho, amrex::Real *  T, amrex::Real *  y, amrex::Real *  qdot)
+{
+    int id; /*loop counter */
+    amrex::Real c[2]; /*temporary storage */
+    amrex::Real imw[2];
+
+    get_imw(imw);
+
+    /*See Eq 8 with an extra 1e6 so c goes to SI */
+    c[0] = 1e6 * (*rho) * y[0]*imw[0]; 
+    c[1] = 1e6 * (*rho) * y[1]*imw[1]; 
+
+    /*call progressRate */
+    progressRate(qdot, c, *T);
+
+    /*convert to chemkin units */
+    for (id = 0; id < 0; ++id) {
+        qdot[id] *= 1.0e-6;
+    }
+}
+
+
+/*Returns the progress rates of each reactions */
+/*Given rho, T, and mole fractions */
+void CKQXR(amrex::Real *  rho, amrex::Real *  T, amrex::Real *  x, amrex::Real *  qdot)
+{
+    int id; /*loop counter */
+    amrex::Real c[2]; /*temporary storage */
+    amrex::Real XW = 0; /*See Eq 4, 11 in CK Manual */
+    amrex::Real ROW; 
+    /*Compute mean molecular wt first */
+    XW += x[0]*31.998800; /*O2 */
+    XW += x[1]*28.013400; /*N2 */
+    /*Extra 1e6 factor to take c to SI */
+    ROW = 1e6*(*rho) / XW;
+
+    /*Compute conversion, see Eq 11 */
+    for (id = 0; id < 2; ++id) {
+        c[id] = x[id]*ROW;
+    }
+
+    /*convert to chemkin units */
+    progressRate(qdot, c, *T);
+
+    /*convert to chemkin units */
+    for (id = 0; id < 0; ++id) {
+        qdot[id] *= 1.0e-6;
+    }
+}
+
+/*compute the reaction Jacobian on CPU */
+void aJacobian_cpu(amrex::Real *  J, amrex::Real *  sc, amrex::Real T, int consP)
+{
+    for (int i=0; i<9; i++) {
+        J[i] = 0.0;
+    }
+
+    amrex::Real wdot[2];
+    for (int k=0; k<2; k++) {
+        wdot[k] = 0.0;
+    }
+
+    amrex::Real tc[] = { log(T), T, T*T, T*T*T, T*T*T*T }; /*temperature cache */
+    amrex::Real invT = 1.0 / tc[1];
+    amrex::Real invT2 = invT * invT;
+
+
+    /*reference concentration: P_atm / (RT) in inverse mol/m^3 */
+    amrex::Real refC = 101325 / 8.31446 / T;
+    amrex::Real refCinv = 1.0 / refC;
+
+    /*compute the mixture concentration */
+    amrex::Real mixture = 0.0;
+    for (int k = 0; k < 2; ++k) {
+        mixture += sc[k];
+    }
+
+    /*compute the Gibbs free energy */
+    amrex::Real g_RT[2];
+    gibbs(g_RT, tc);
+
+    /*compute the species enthalpy */
+    amrex::Real h_RT[2];
+    speciesEnthalpy(h_RT, tc);
+
+    amrex::Real phi_f, k_f, k_r, phi_r, Kc, q, q_nocor, Corr, alpha;
+    amrex::Real dlnkfdT, dlnk0dT, dlnKcdT, dkrdT, dqdT;
+    amrex::Real dqdci, dcdc_fac, dqdc[2];
+    amrex::Real Pr, fPr, F, k_0, logPr;
+    amrex::Real logFcent, troe_c, troe_n, troePr_den, troePr, troe;
+    amrex::Real Fcent1, Fcent2, Fcent3, Fcent;
+    amrex::Real dlogFdc, dlogFdn, dlogFdcn_fac;
+    amrex::Real dlogPrdT, dlogfPrdT, dlogFdT, dlogFcentdT, dlogFdlogPr, dlnCorrdT;
+    const amrex::Real ln10 = log(10.0);
+    const amrex::Real log10e = 1.0/log(10.0);
+    amrex::Real c_R[2], dcRdT[2], e_RT[2];
+    amrex::Real * eh_RT;
+    if (consP) {
+        cp_R(c_R, tc);
+        dcvpRdT(dcRdT, tc);
+        eh_RT = &h_RT[0];
+    } else {
+        cv_R(c_R, tc);
+        dcvpRdT(dcRdT, tc);
+        speciesInternalEnergy(e_RT, tc);
+        eh_RT = &e_RT[0];
+    }
+
+    amrex::Real cmix = 0.0, ehmix = 0.0, dcmixdT=0.0, dehmixdT=0.0;
+    for (int k = 0; k < 2; ++k) {
+        cmix += c_R[k]*sc[k];
+        dcmixdT += dcRdT[k]*sc[k];
+        ehmix += eh_RT[k]*wdot[k];
+        dehmixdT += invT*(c_R[k]-eh_RT[k])*wdot[k] + eh_RT[k]*J[6+k];
+    }
+
+    amrex::Real cmixinv = 1.0/cmix;
+    amrex::Real tmp1 = ehmix*cmixinv;
+    amrex::Real tmp3 = cmixinv*T;
+    amrex::Real tmp2 = tmp1*tmp3;
+    amrex::Real dehmixdc;
+    /* dTdot/d[X] */
+    for (int k = 0; k < 2; ++k) {
+        dehmixdc = 0.0;
+        for (int m = 0; m < 2; ++m) {
+            dehmixdc += eh_RT[m]*J[k*3+m];
+        }
+        J[k*3+2] = tmp2*c_R[k] - tmp3*dehmixdc;
+    }
+    /* dTdot/dT */
+    J[8] = -tmp1 + tmp2*dcmixdT - tmp3*dehmixdT;
+}
+
+#endif
+
+
+/* Initializes parameter database */
+void CKINIT()
+{
+#ifndef AMREX_USE_GPU
+#endif
+}
+
+
+/* Finalizes parameter database */
+void CKFINALIZE()
+{
+#ifndef AMREX_USE_GPU
+    for (int i=0; i<0; ++i) {
+        free(TB[i]); TB[i] = 0; 
+        free(TBid[i]); TBid[i] = 0;
+        nTB[i] = 0;
+    }
+#endif
+}
 
 
 /*save atomic weights into array */
@@ -77,7 +446,7 @@ void SPARSITY_INFO( int * nJdata, int * consP, int NCELLS)
             for (int l=0; l<2; l++) {
                 c_d[l] = 1.0/ 2.000000 ;
             }
-            aJacobian(J_d, c_d, 1500.0, *consP);
+            aJacobian_cpu(J_d, c_d, 1500.0, *consP);
     });
 
 #ifdef AMREX_USE_GPU
@@ -118,7 +487,7 @@ void SPARSITY_INFO_SYST( int * nJdata, int * consP, int NCELLS)
             for (int k=0; k<2; k++) {
                 c_d[k] = 1.0/ 2.000000 ;
             }
-            aJacobian(J_d, c_d, 1500.0, *consP);
+            aJacobian_cpu(J_d, c_d, 1500.0, *consP);
     });
 
 #ifdef AMREX_USE_GPU
@@ -210,7 +579,7 @@ void SPARSITY_PREPROC_CSC(int *  rowVals, int *  colPtrs, int * consP, int NCELL
             for (int k=0; k<2; k++) {
                 c_d[k] = 1.0/ 2.000000 ;
             }
-            aJacobian(J_d, c_d, 1500.0, *consP);
+            aJacobian_cpu(J_d, c_d, 1500.0, *consP);
     });
 
 #ifdef AMREX_USE_GPU
@@ -255,7 +624,7 @@ void SPARSITY_PREPROC_CSR(int * colVals, int * rowPtrs, int * consP, int NCELLS,
             for (int k=0; k<2; k++) {
                 c_d[k] = 1.0/ 2.000000 ;
             }
-            aJacobian(J_d, c_d, 1500.0, *consP);
+            aJacobian_cpu(J_d, c_d, 1500.0, *consP);
     });
 
 #ifdef AMREX_USE_GPU
@@ -317,7 +686,7 @@ void SPARSITY_PREPROC_SYST_CSR(int * colVals, int * rowPtr, int * consP, int NCE
             for (int k=0; k<2; k++) {
                 c_d[k] = 1.0/ 2.000000 ;
             }
-            aJacobian(J_d, c_d, 1500.0, *consP);
+            aJacobian_cpu(J_d, c_d, 1500.0, *consP);
     });
 
 #ifdef AMREX_USE_GPU
@@ -484,4 +853,3 @@ void SPARSITY_PREPROC_SYST_SIMPLIFIED_CSR(int * colVals, int * rowPtr, int * con
 }
 
 #endif
-
