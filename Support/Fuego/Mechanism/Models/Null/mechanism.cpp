8980fbabb4f4d92b0220d6ff36e13db25b62676c
Anne Felden <amfelden@lake.lbl.gov>
2021-04-26 13:27:16 -0700

    Update Null chem reaction folder

--- Support/Fuego/Mechanism/Models/Null/mechanism.cpp
+++ Support/Fuego/Mechanism/Models/Null/mechanism.cpp
@@ -1,25 +1,13 @@
-#include "chemistry_file.H"
+#ifndef MECHANISM_CPP
+#define MECHANISM_CPP
+
+#include "mechanism.H"
+
 
 void CKSYMS_STR(amrex::Vector<std::string>& kname) {}
 
-/* Returns R, Rc, Patm */
-void CKRP(amrex::Real *  ru, amrex::Real *  ruc, amrex::Real *  pa)
-{
-     *ru  = 8.31446261815324e+07; 
-     *ruc = 1.98721558317399615845; 
-     *pa  = 1.01325e+06; 
-}
+void CKINIT() {}
 
-AMREX_GPU_HOST_DEVICE void CKINIT()
-{
-}
+void CKFINALIZE() {}
 
-AMREX_GPU_HOST_DEVICE void CKFINALIZE()
-{
-}
-
-AMREX_GPU_HOST_DEVICE void CKWC(amrex::Real *  T, amrex::Real *  C,  amrex::Real *  wdot)
-{
-     wdot[0] = 0.0e0;
-}
-
+#endif
