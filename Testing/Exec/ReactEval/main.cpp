42f1c636f209433c83e2dbaefccedafe8530c031
Lucas Esclapez <13371051+esclapez@users.noreply.github.com>
2021-07-16 15:54:38 -0600

    Fix the Testing/Exec/ReactEval with recent changes. (#166)
    * Update the Testing/Exec/ReactEval from Testing/Exec/ReactEval_C
    
    * Remove old ReactEval_C
    
    * Add input to test Array4 and 1dArray version of react().

--- Testing/Exec/ReactEval/main.cpp
+++ Testing/Exec/ReactEval/main.cpp
@@ -15,7 +15,7 @@
 
 #include <PlotFileFromMF.H>
 #include <PelePhysics.H>
-#include <reactor.h>
+#include <reactor.H>
 
 #ifndef USE_RK64_PP
 #ifdef USE_ARKODE_PP 
@@ -38,6 +38,9 @@ main (int   argc,
   amrex::sundials::MemoryHelper::Initialize(); /* TODO: this ideally (I think) will go in the amrex::Initialize */
 #endif
   {
+
+    Real strt_time = ParallelDescriptor::second();
+
     BL_PROFILE_VAR("main::main()", pmain);
 
     ParmParse pp;
@@ -51,10 +54,24 @@ main (int   argc,
       do_plt = true;
     }
 
+    /* react() function version */
+    // 1 -> Array4 version of react()  (Default)
+    // 2 -> 1d raw pointer version of react()
+    std::string reactFormat = "Array4";
+    int reactFunc;
+    pp.query("reactFormat", reactFormat);
+    if ( reactFormat == "Array4") {
+       reactFunc = 1;
+    } else if ( reactFormat == "1dArray" ) {
+       reactFunc = 2;
+    } else {
+       Abort(" --> reactFormat can only be 'Array4' or '1dArray' !");
+    }
+
     /* ODE inputs */
     ParmParse ppode("ode");
     int ode_ncells = 1;
-    ppode.query("ode_ncells",ode_ncells); // number of cells to integrate per call
+    ppode.query("ode_ncells",ode_ncells); // number of cells to integrate per call, used only if reactFunc = 2
 
     Real dt = 1.e-5;
     ppode.query("dt",dt);
@@ -99,9 +116,8 @@ main (int   argc,
     pele::physics::transport::InitTransport<
       pele::physics::PhysicsType::eos_type>()();
 
-    BL_PROFILE_VAR("main::reactor_info()", reactInfo);
-
     /* Initialize reactor object inside OMP region, including tolerances */
+    BL_PROFILE_VAR("main::reactor_info()", reactInfo);
 #ifdef _OPENMP
 #pragma omp parallel if (Gpu::notInLaunchRegion())
 #endif
@@ -119,7 +135,9 @@ main (int   argc,
     }
     BL_PROFILE_VAR_STOP(reactInfo);
 
+    // -----------------------------------------------------------------------------
     // Define geometry
+    // -----------------------------------------------------------------------------
     std::array<int,3> ncells = {AMREX_D_DECL(1,1,1)};
     if (pp.countval("ncells") == 1) {
       pp.get("ncells",ncells[0]);
@@ -137,7 +155,6 @@ main (int   argc,
     Box domain(IntVect(AMREX_D_DECL(0,0,0)),
                IntVect(AMREX_D_DECL(ncells[0]-1,ncells[1]-1,ncells[2]-1)));
 
-    Print() << "Integrating "<< domain.numPts() << " cells for: " << dt << " seconds" << std::endl;
 
     RealBox real_box({AMREX_D_DECL(-1.0,-1.0,-1.0)},
                      {AMREX_D_DECL( 1.0, 1.0, 1.0)});
@@ -148,16 +165,21 @@ main (int   argc,
 
     Geometry geom(domain, real_box, coord, is_periodic);
 
-    // Define BoxArray
+    Print() << "Integrating "<< domain.numPts() << " cells for: " << dt << " seconds with " << ndt << " substeps \n";
+
+    // -----------------------------------------------------------------------------
+    // Define BoxArray / Dmap
+    // -----------------------------------------------------------------------------
     int max_grid_size = 16;
     pp.query("max_grid_size",max_grid_size);
     BoxArray ba(domain);
     ba.maxSize(max_grid_size);
-
     DistributionMapping dm(ba);
-    int num_grow = 0;
 
-    /* Create MultiFabs with no ghost cells */
+    // -----------------------------------------------------------------------------
+    // Create MultiFabs with no ghost cells
+    // -----------------------------------------------------------------------------
+    int num_grow = 0;
     MultiFab mf(ba, dm, NUM_SPECIES + 1, num_grow);
     MultiFab rY_source_ext(ba,dm,NUM_SPECIES, num_grow);
     MultiFab mfE(ba, dm, 1, num_grow);
@@ -169,22 +191,24 @@ main (int   argc,
 
     FabArrayBase::mfiter_tile_size = IntVect(AMREX_D_DECL(1024,1024,1024));
 
-    /* INIT */
+    // -----------------------------------------------------------------------------
+    // Initialize data
+    // -----------------------------------------------------------------------------
     BL_PROFILE_VAR("main::initialize_data()", InitData);
     const auto geomdata = geom.data();
-#ifdef _OPENMP
+#ifdef AMREX_USE_OMP
 #pragma omp parallel if (Gpu::notInLaunchRegion())
 #endif
     for (MFIter mfi(mf,TilingIfNotGPU()); mfi.isValid(); ++mfi) {
 
-      const Box& gbox = mfi.tilebox();
+      const Box& box = mfi.tilebox();
 
       Array4<Real> const& rY_a    = mf.array(mfi);
       Array4<Real> const& rYs_a   = rY_source_ext.array(mfi);
       Array4<Real> const& E_a     = mfE.array(mfi);
       Array4<Real> const& rE_a    = rY_source_energy_ext.array(mfi);
 
-      ParallelFor(gbox,
+      ParallelFor(box,
       [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
       {
         initialize_data(i, j, k, fuel_idx, 
@@ -194,7 +218,19 @@ main (int   argc,
     }
     BL_PROFILE_VAR_STOP(InitData);
 
-#ifdef _OPENMP
+    // Print initial data if needed
+    BL_PROFILE_VAR_NS("PlotFile",PlotFile);
+    if (do_plt) {
+      BL_PROFILE_VAR_START(PlotFile);
+      std::string outfile = Concatenate(pltfile,0);
+      PlotFileFromMF(mf,outfile);
+      BL_PROFILE_VAR_STOP(PlotFile);
+    }
+
+    // -----------------------------------------------------------------------------
+    // Set typical values
+    // -----------------------------------------------------------------------------
+#ifdef AMREX_USE_OMP
 #pragma omp parallel if (Gpu::notInLaunchRegion())
 #endif
     {
@@ -209,14 +245,6 @@ main (int   argc,
       }
     }
 
-    BL_PROFILE_VAR_NS("PlotFile",PlotFile);
-    if (do_plt) {
-      BL_PROFILE_VAR_START(PlotFile);
-      std::string outfile = Concatenate(pltfile,0);
-      PlotFileFromMF(mf,outfile);
-      BL_PROFILE_VAR_STOP(PlotFile);
-    }
-
     Print() << " \n STARTING THE ADVANCE \n";
 
     /* REACT */
@@ -224,7 +252,7 @@ main (int   argc,
     BL_PROFILE_VAR_NS("React",ReactInLoop);
     BL_PROFILE_VAR_NS("Allocs",Allocs);
     BL_PROFILE_VAR_NS("Flatten",mainflatten);
-#ifdef _OPENMP
+#ifdef AMREX_USE_OMP
     const auto tiling = MFItInfo().SetDynamic(true);
 #pragma omp parallel
 #else
@@ -247,123 +275,110 @@ main (int   argc,
       auto const& fc      = fctCount.array(mfi);
       auto const& mask    = dummyMask.array(mfi);
 
-#ifdef AMREX_USE_GPU
-      ode_ncells    = nc;
-#else
-#ifndef CVODE_BOXINTEG
-      extra_cells = nc - (nc / ode_ncells) * ode_ncells; 
-#else
-      ode_ncells    = nc;
-#endif
-#endif
-
-      Print() << " Integrating " << nc << " cells with a "<<ode_ncells<< " ode cell buffer \n";
-      Print() << "("<< extra_cells<<" extra cells) \n";
-
-#ifndef CVODE_BOXINTEG
-      BL_PROFILE_VAR_START(Allocs);
-      int nCells               =  nc+extra_cells;
-      auto tmp_vect            =  new Real[nCells * (NUM_SPECIES+1)];
-      auto tmp_src_vect        =  new Real[nCells * NUM_SPECIES];
-      auto tmp_vect_energy     =  new Real[nCells];
-      auto tmp_src_vect_energy =  new Real[nCells];
-      auto tmp_fc              =  new Real[nCells];
-      auto tmp_mask            =  new int[nCells];
-      BL_PROFILE_VAR_STOP(Allocs);
-
-      BL_PROFILE_VAR_START(mainflatten);
-      ParallelFor(box,
-      [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept
-      {
-        int icell = (k-lo.z)*len.x*len.y + (j-lo.y)*len.x + (i-lo.x);
-        for(int sp=0; sp<NUM_SPECIES; sp++) {
-          tmp_vect[icell*(NUM_SPECIES+1)+sp]     = rhoY(i,j,k,sp);
-          tmp_src_vect[icell*NUM_SPECIES+sp]     = frcExt(i,j,k,sp);
-        }
-        tmp_vect[icell*(NUM_SPECIES+1)+NUM_SPECIES] = T(i,j,k);
-        tmp_vect_energy[icell]                      = rhoE(i,j,k);
-        tmp_src_vect_energy[icell]                  = frcEExt(i,j,k);
-        tmp_mask[icell]                             = mask(i,j,k);
-      });
-
-      for (int icell=nc; icell<nc+extra_cells; icell++) {
-        for(int sp=0; sp<NUM_SPECIES; sp++) {
-          tmp_vect[icell*(NUM_SPECIES+1)+sp]     = rhoY(0,0,0,sp);
-          tmp_src_vect[icell*NUM_SPECIES+sp]     = frcExt(0,0,0,sp);
-        }
-        tmp_vect[icell*(NUM_SPECIES+1)+NUM_SPECIES] = T(0,0,0);
-        tmp_vect_energy[icell]                      = rhoE(0,0,0); 
-        tmp_src_vect_energy[icell]                  = frcEExt(0,0,0);
-        tmp_mask[icell]                             = mask(0,0,0);
-      }
-      BL_PROFILE_VAR_STOP(mainflatten);
-#endif
-
-      /* Solve */
-      BL_PROFILE_VAR_START(ReactInLoop);
-#ifndef CVODE_BOXINTEG
-      for(int i = 0; i < nCells; i+=ode_ncells) {
-        if (tmp_mask[i]==1)
-        {
-          tmp_fc[i] = 0.0;
-          Real time = 0.0;
-          Real dt_incr = dt/ndt;
-          for (int ii = 0; ii < ndt; ++ii) {
-            tmp_fc[i] += react(tmp_vect + i*(NUM_SPECIES+1), tmp_src_vect + i*NUM_SPECIES,
-                               tmp_vect_energy + i, tmp_src_vect_energy + i,
-                               dt_incr, time);
-            dt_incr =  dt/ndt;
-          }
-        }
-      }
-#else
-      {      
+      // -------------------------------------------------------------
+      // Integration with Array4 react function
+      if (reactFunc == 1) {
         Real time = 0.0;
         Real dt_incr = dt/ndt;
+
+        Print() << " Integrating " << nc << " cells \n";
+        /* Solve */
+        BL_PROFILE_VAR_START(ReactInLoop);
         for (int ii = 0; ii < ndt; ++ii)
         {
+          react(box, rhoY, frcExt, T,
+                rhoE, frcEExt, fc, mask,
+                dt_incr, time, ode_iE
 #ifdef AMREX_USE_GPU
-          react(box,
-                rhoY, frcExt, T,
-                rhoE, frcEExt,
-                fc, mask,
-                dt_incr, time,
-                ode_iE, amrex::Gpu::gpuStream());
-#else
-          react(box,
-                  rhoY, frcExt, T,
-                  rhoE, frcEExt,
-                  fc, mask,
-                  dt_incr, time);
+                , amrex::Gpu::gpuStream()
 #endif
+               );
           dt_incr =  dt/ndt;
         }
-      }
-#endif
-      BL_PROFILE_VAR_STOP(ReactInLoop);
-      
-#ifndef CVODE_BOXINTEG
-      BL_PROFILE_VAR_START(mainflatten);
-      ParallelFor(box,
-      [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept
-      {
-        int icell = (k-lo.z)*len.x*len.y + (j-lo.y)*len.x + (i-lo.x);
-        for(int sp=0; sp<NUM_SPECIES; sp++) {
-          rhoY(i,j,k,sp)        = tmp_vect[icell*(NUM_SPECIES+1)+sp];
-        }
-        rhoY(i,j,k,NUM_SPECIES) = tmp_vect[icell*(NUM_SPECIES+1) + NUM_SPECIES];
-        rhoE(i,j,k)             = tmp_vect_energy[icell];
-        fc(i,j,k)               = tmp_fc[icell];
-      });
-      BL_PROFILE_VAR_STOP(mainflatten);
+        BL_PROFILE_VAR_STOP(ReactInLoop);
 
-      delete[] tmp_vect;
-      delete[] tmp_src_vect;
-      delete[] tmp_vect_energy;
-      delete[] tmp_src_vect_energy;
-      delete[] tmp_fc;
-      delete[] tmp_mask;
+      // -------------------------------------------------------------
+      // Integration with 1dArray raw pointer react function
+      } else if (reactFunc == 2) {
+
+          // On GPU, integrate the entirely box at once
+          // othewise use the user-input ode_ncells
+#ifdef AMREX_USE_GPU
+          ode_ncells    = nc;
 #endif
+          extra_cells = nc - (nc / ode_ncells) * ode_ncells;
+          Print() << " Integrating " << nc << " cells with a "<< ode_ncells << " ode cell buffer ";
+          Print() << "("<< extra_cells<<" extra cells) \n";
+
+          BL_PROFILE_VAR_START(Allocs);
+          int nCells               =  nc+extra_cells;
+          auto tmp_vect            =  new Real[nCells * (NUM_SPECIES+1)];
+          auto tmp_src_vect        =  new Real[nCells * NUM_SPECIES];
+          auto tmp_vect_energy     =  new Real[nCells];
+          auto tmp_src_vect_energy =  new Real[nCells];
+          auto tmp_fc              =  new int[nCells];
+          auto tmp_mask            =  new int[nCells];
+          BL_PROFILE_VAR_STOP(Allocs);
+
+          BL_PROFILE_VAR_START(mainflatten);
+          ParallelFor(box,
+          [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept
+          {
+            int icell = (k-lo.z)*len.x*len.y + (j-lo.y)*len.x + (i-lo.x);
+
+            box_flatten(icell, i, j, k, ode_iE, 
+                        rhoY, frcExt, T, rhoE, frcEExt, mask,
+                        tmp_vect, tmp_src_vect, tmp_vect_energy, tmp_src_vect_energy, tmp_mask);
+          });
+
+          for (int icell=nc; icell<nc+extra_cells; icell++) {
+            for(int sp=0; sp<NUM_SPECIES; sp++) {
+              tmp_vect[icell*(NUM_SPECIES+1)+sp]     = rhoY(0,0,0,sp);
+              tmp_src_vect[icell*NUM_SPECIES+sp]     = frcExt(0,0,0,sp);
+            }
+            tmp_vect[icell*(NUM_SPECIES+1)+NUM_SPECIES] = T(0,0,0);
+            tmp_vect_energy[icell]                      = rhoE(0,0,0); 
+            tmp_src_vect_energy[icell]                  = frcEExt(0,0,0);
+            tmp_mask[icell]                             = mask(0,0,0);
+          }
+          BL_PROFILE_VAR_STOP(mainflatten);
+
+          /* Solve */
+          BL_PROFILE_VAR_START(ReactInLoop);
+          for(int i = 0; i < nCells; i+=ode_ncells) {
+             tmp_fc[i] = 0;
+             Real time = 0.0;
+             Real dt_incr = dt/ndt;
+             for (int ii = 0; ii < ndt; ++ii) {
+               tmp_fc[i] += react(&tmp_vect[i*(NUM_SPECIES+1)], &tmp_src_vect[i*NUM_SPECIES],
+                                  &tmp_vect_energy[i], &tmp_src_vect_energy[i],
+                                  dt_incr,time);
+               dt_incr =  dt/ndt;
+               for (int ic = i+1; ic < i+ode_ncells ; ++ic) {
+                  tmp_fc[ic] = tmp_fc[i];
+               }
+             }
+          }
+          BL_PROFILE_VAR_STOP(ReactInLoop);
+      
+          BL_PROFILE_VAR_START(mainflatten);
+          ParallelFor(box,
+          [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept
+          {
+            int icell = (k-lo.z)*len.x*len.y + (j-lo.y)*len.x + (i-lo.x);
+            box_unflatten(icell, i, j, k, ode_iE,
+                          rhoY, T, rhoE, frcEExt, fc,
+                          tmp_vect, tmp_vect_energy, tmp_fc, dt);
+          });
+          BL_PROFILE_VAR_STOP(mainflatten);
+
+          delete[] tmp_vect;
+          delete[] tmp_src_vect;
+          delete[] tmp_vect_energy;
+          delete[] tmp_src_vect_energy;
+          delete[] tmp_fc;
+          delete[] tmp_mask;
+      }
     }
     BL_PROFILE_VAR_STOP(Advance);
 
@@ -377,23 +392,23 @@ main (int   argc,
     }
 
     if (do_plt) {
-      BL_PROFILE_VAR_START(PlotFile);
-      MultiFab out(mf.boxArray(),mf.DistributionMap(),mf.nComp()+1,mf.nGrow());
-      MultiFab::Copy(out,mf,0,0,mf.nComp(),mf.nGrow());
-      AMREX_ALWAYS_ASSERT(fctCount.boxArray()==mf.boxArray() && fctCount.nGrow()>=mf.nGrow());
-      MultiFab::Copy(out,fctCount,0,mf.nComp(),1,mf.nGrow());
-
-      PlotFileFromMF(out,Concatenate(pltfile,1));
-      BL_PROFILE_VAR_STOP(PlotFile);
+        BL_PROFILE_VAR_START(PlotFile);
+        MultiFab out(mf.boxArray(),mf.DistributionMap(),mf.nComp()+1,mf.nGrow());
+        MultiFab::Copy(out,mf,0,0,mf.nComp(),mf.nGrow());
+        AMREX_ALWAYS_ASSERT(fctCount.boxArray()==mf.boxArray() && fctCount.nGrow()>=mf.nGrow());
+        MultiFab::Copy(out,fctCount,0,mf.nComp(),1,mf.nGrow());
+        PlotFileFromMF(out,Concatenate(pltfile,1));
+        BL_PROFILE_VAR_STOP(PlotFile);
     }
     
-#ifndef AMREX_USE_GPU
-    reactor_close();
-#endif
     pele::physics::transport::CloseTransport<
       pele::physics::PhysicsType::eos_type>()();
 
     BL_PROFILE_VAR_STOP(pmain);
+
+    Real run_time = ParallelDescriptor::second() - strt_time;
+    ParallelDescriptor::ReduceRealMax(run_time, ParallelDescriptor::IOProcessorNumber());
+    amrex::Print() << " \n >> React::main() " << run_time << "\n\n";
   }
   Finalize();
 
