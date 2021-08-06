8fc98c23744fcdd643d5cb0b9fc6b29623422556
Hariswaran Sitaraman <hsitaram@el2.ib0.cm.hpc.nrel.gov>
2021-08-06 15:20:05 -0600

    clang-formatting

--- Reactions/reactor.cpp
+++ Reactions/reactor.cpp
@@ -1,11 +1,11 @@
 #include "reactor.H"
 
-amrex::Array<double, NUM_SPECIES + 1> typVals={-1};
-int eint_rho=1; // in/out = rhoE/rhoY
-int enth_rho=2; // in/out = rhoH/rhoY
-amrex::Real relTol=1e-6;
-amrex::Real absTol=1e-10;
-amrex::Real time_init=0.0;
+amrex::Array<double, NUM_SPECIES + 1> typVals = {-1};
+int eint_rho = 1; // in/out = rhoE/rhoY
+int enth_rho = 2; // in/out = rhoH/rhoY
+amrex::Real relTol = 1e-6;
+amrex::Real absTol = 1e-10;
+amrex::Real time_init = 0.0;
 int dense_solve = 1;
 int sparse_solve = 5;
 int iterative_gmres_solve = 99;
@@ -17,11 +17,12 @@ int use_erkstep = 0;
 int rk_method = 40;
 int rk_controller = 0;
 int rk64_nsubsteps_guess = 10;
-int rk64_nsubsteps_min   = 5;
-int rk64_nsubsteps_max   = 500;
+int rk64_nsubsteps_min = 5;
+int rk64_nsubsteps_max = 500;
 
 //===================================================================
-void SetTypValsODE(const std::vector<amrex::Real>& ExtTypVals)
+void
+SetTypValsODE(const std::vector<amrex::Real>& ExtTypVals)
 {
   int size_ETV = ExtTypVals.size();
   amrex::Vector<std::string> kname;
@@ -32,25 +33,22 @@ void SetTypValsODE(const std::vector<amrex::Real>& ExtTypVals)
   omp_thread = omp_get_thread_num();
 #endif
 
-  for (int i = 0; i < size_ETV - 1; i++) 
-  {
+  for (int i = 0; i < size_ETV - 1; i++) {
     typVals[i] = ExtTypVals[i];
   }
   typVals[size_ETV - 1] = ExtTypVals[size_ETV - 1];
 
-  if (omp_thread == 0) 
-  {
+  if (omp_thread == 0) {
     amrex::Print() << "Set the typVals in PelePhysics: \n  ";
-    for (int i = 0; i < size_ETV - 1; i++) 
-    {
+    for (int i = 0; i < size_ETV - 1; i++) {
       amrex::Print() << kname[i] << ":" << typVals[i] << "  ";
     }
     amrex::Print() << "Temp:" << typVals[size_ETV - 1] << " \n";
   }
-
 }
 //===================================================================
-void SetTolFactODE(amrex::Real relative_tol, amrex::Real absolute_tol)
+void
+SetTolFactODE(amrex::Real relative_tol, amrex::Real absolute_tol)
 {
   int omp_thread = 0;
 #ifdef _OPENMP
@@ -58,11 +56,10 @@ void SetTolFactODE(amrex::Real relative_tol, amrex::Real absolute_tol)
 #endif
   relTol = relative_tol;
   absTol = absolute_tol;
-  if(omp_thread == 0){
-  amrex::Print() << "Set RTOL, ATOL = " << relTol << " " << absTol
-          << " in PelePhysics\n";
+  if (omp_thread == 0) {
+    amrex::Print() << "Set RTOL, ATOL = " << relTol << " " << absTol
+                   << " in PelePhysics\n";
   }
-
 }
 //===================================================================
 // Check function return value...
@@ -72,7 +69,8 @@ void SetTolFactODE(amrex::Real relative_tol, amrex::Real absolute_tol)
 //              flag >= 0
 //     opt == 2 means function allocates memory so check if returned
 //              NULL pointer
-int check_flag(void* flagvalue, const char* funcname, int opt)
+int
+check_flag(void* flagvalue, const char* funcname, int opt)
 {
   int* errflag;
 
