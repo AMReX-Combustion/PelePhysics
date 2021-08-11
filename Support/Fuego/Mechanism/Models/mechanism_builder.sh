2ab1f9a995f039782589388fe6dd740aacae0d6c
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2021-08-10 19:24:39 -0600

    Remove use of realpath

--- Support/Fuego/Mechanism/Models/mechanism_builder.sh
+++ Support/Fuego/Mechanism/Models/mechanism_builder.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 
+function abspath() {
+    pushd . > /dev/null;
+    if [ -d "$1" ]; then
+        cd "$1" || exit;
+        dirs -l +0;
+    else
+        cd "$(dirname "$1")" || exit;
+        cur_dir=$(dirs -l +0);
+        if [ "$cur_dir" == "/" ]; then
+            echo "$cur_dir$(basename "$1")";
+        else
+            echo "$cur_dir/$(basename "$1")";
+        fi;
+    fi;
+    popd > /dev/null || exit;
+}
+
 if [ -z "${PELE_PHYSICS_HOME+xxx}" ]; then
-    PELE_PHYSICS_HOME="$(realpath ../../../../../)"
+    PELE_PHYSICS_HOME="$(abspath ../../../../..)"
     echo "Using PELE_PHYSICS_HOME: ${PELE_PHYSICS_HOME}. Set PELE_PHYSICS_HOME you want a different one."
+else
+    echo "Using PELE_PHYSICS_HOME: ${PELE_PHYSICS_HOME}"
 fi
 
 export FMC="${PELE_PHYSICS_HOME}/Support/Fuego/Pythia/products/bin/fmc.py"
@@ -11,4 +30,6 @@ export HEADERDIR="${PELE_PHYSICS_HOME}/Support/Fuego/Mechanism/Models/header"
 if [ -z "${FUEGO_PYTHON+xxx}" ]; then
     export FUEGO_PYTHON=$(which python)
     echo "Using environment python: $FUEGO_PYTHON. Set FUEGO_PYTHON if you want a different one."
+else
+    echo "Using FUEGO_PYTHON: $FUEGO_PYTHON"
 fi
