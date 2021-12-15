3380cbe01a52decf7e527b849815956903bf19f2
Malik <malik.hassanaly@gmail.com>
2021-12-15 14:17:30 -0700

    Added dodecane QSS test. Works for 1 timestep

---
+++ Testing/Exec/ReactEval_qss/exec.sh
@@ -0,0 +1,28 @@
+# Minimal Clean
+if test -f "PPreaction.txt"; then
+    rm PPreaction.txt
+fi
+if test -f "Pele3d.llvm.ex"; then
+    rm Pele3d.llvm.ex
+fi
+
+# Compile
+make COMP=llvm TPL
+make COMP=llvm
+
+# Execute 0D
+./Pele3d.llvm.ex inputs/inputsRK64
+
+# Compare to Cantera
+if test -f "compareCantera/canteraSim/output/CanteraReaction_dodecane.txt"; then
+    cd compareCantera
+    python compareResults.py
+    cd ..
+else
+    source ~/miniconda/bin/activate cantera
+    cd compareCantera/canteraSim
+    python homoReact_dodecane.py  
+    cd ../
+    python compareResults.py
+    cd ..
+fi
