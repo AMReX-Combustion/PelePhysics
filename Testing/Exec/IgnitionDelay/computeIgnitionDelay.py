b1b5095653a69d874236ff6ca8949d924a29cff7
Malik <malik.hassanaly@gmail.com>
2022-04-20 09:26:13 -0600

    Protect main script execution

--- Testing/Exec/IgnitionDelay/computeIgnitionDelay.py
+++ Testing/Exec/IgnitionDelay/computeIgnitionDelay.py
@@ -58,40 +58,45 @@ def readMultiColFile(filename,headerSize=0):
         counter=counter+1
     return Array
 
-# CLI
-parser = argparse.ArgumentParser(description='Compute ignition delay')
-group = parser.add_mutually_exclusive_group()
-parser.add_argument('-f', '--inputFile', type=str, metavar='', required=False, help='Pele input file', default='')
-group.add_argument('-est','--estimate', action='store_true', help='first estimate')
-group.add_argument('-ref','--refine', action='store_true', help='Second estimate')
-group2 = parser.add_mutually_exclusive_group()
-group2.add_argument('-v','--verbose', action='store_true', help='verbose')
-group2.add_argument('-q','--quiet', action='store_true', help='quiet')
-args = parser.parse_args()
 
 
-# Read Pele input file
-inpt = parseInputFile(args.inputFile)
-dt = float(inpt['ode.dt'])
-ndt = float(inpt['ode.ndt'])
-press = float(inpt['hr.press'])
-temp = float(inpt['hr.t0'])
-phi = float(inpt['hr.equiv_ratio'])
 
+if __name__=="__main__":
 
-# Process result
-filePP = 'PPreaction.txt'
-A1 = readMultiColFile(filePP)
-ignitionDelayPP = (dt/ndt)*np.amin(np.argwhere(A1[:,1]>(A1[0,1]+400)))
-
-
-if args.estimate:
-    if args.verbose: 
-        print("Rough estimate: T = %.3g K , P = %.3g atm, Phi = %.3g,  tIgn = %.3g +/- %.3g s " % (temp,press/101325.0,phi,ignitionDelayPP,dt/ndt))
-    # Write new input file
-    writeNewInput(args.inputFile,'inputs/inputs.0d_refine',ignitionDelayPP*1.1,ndt*2)
-
-if args.refine:
-    print("T = %.3g K , P = %.3g atm, Phi = %.3g,  tIgn = %.3g +/- %.3g s " % (temp,press/101325.0,phi,ignitionDelayPP,dt/ndt))
-    print("Equivalence ratio calculation assumed you are using Ndodecane")
-    print("Change GPU_misc.H if you are not")
+    # CLI
+    parser = argparse.ArgumentParser(description='Compute ignition delay')
+    group = parser.add_mutually_exclusive_group()
+    parser.add_argument('-f', '--inputFile', type=str, metavar='', required=False, help='Pele input file', default='')
+    group.add_argument('-est','--estimate', action='store_true', help='first estimate')
+    group.add_argument('-ref','--refine', action='store_true', help='Second estimate')
+    group2 = parser.add_mutually_exclusive_group()
+    group2.add_argument('-v','--verbose', action='store_true', help='verbose')
+    group2.add_argument('-q','--quiet', action='store_true', help='quiet')
+    args = parser.parse_args()
+    
+    
+    # Read Pele input file
+    inpt = parseInputFile(args.inputFile)
+    dt = float(inpt['ode.dt'])
+    ndt = float(inpt['ode.ndt'])
+    press = float(inpt['hr.press'])
+    temp = float(inpt['hr.t0'])
+    phi = float(inpt['hr.equiv_ratio'])
+    
+    
+    # Process result
+    filePP = 'PPreaction.txt'
+    A1 = readMultiColFile(filePP)
+    ignitionDelayPP = (dt/ndt)*np.amin(np.argwhere(A1[:,1]>(A1[0,1]+400)))
+    
+    
+    if args.estimate:
+        if args.verbose: 
+            print("Rough estimate: T = %.3g K , P = %.3g atm, Phi = %.3g,  tIgn = %.3g +/- %.3g s " % (temp,press/101325.0,phi,ignitionDelayPP,dt/ndt))
+        # Write new input file
+        writeNewInput(args.inputFile,'inputs/inputs.0d_refine',ignitionDelayPP*1.1,ndt*2)
+    
+    if args.refine:
+        print("T = %.3g K , P = %.3g atm, Phi = %.3g,  tIgn = %.3g +/- %.3g s " % (temp,press/101325.0,phi,ignitionDelayPP,dt/ndt))
+        print("Equivalence ratio calculation assumed you are using Ndodecane")
+        print("Change GPU_misc.H if you are not")
