4863baf9105143d8509eeb1b63f6dec7d50be4a5
Jon Rood <jon.rood@nrel.gov>
2022-05-18 13:42:51 -0600

    Make CI consistent with other projects (#277)
    * Make CI consistent with other projects.
    
    * Use https for HIP binaries.

--- .github/workflows/dependencies/dependencies_hip.sh
+++ .github/workflows/dependencies/dependencies_hip.sh
@@ -12,9 +12,9 @@
 
 set -eu -o pipefail
 
-sudo wget http://repo.radeon.com/rocm/rocm.gpg.key
+sudo wget https://repo.radeon.com/rocm/rocm.gpg.key
 sudo apt-key add rocm.gpg.key
-echo 'deb [arch=amd64] http://repo.radeon.com/rocm/apt/debian/ ubuntu main' \
+echo 'deb [arch=amd64] https://repo.radeon.com/rocm/apt/debian/ ubuntu main' \
   | sudo tee /etc/apt/sources.list.d/rocm.list
 echo 'export PATH=/opt/rocm/llvm/bin:/opt/rocm/bin:/opt/rocm/profiler/bin:/opt/rocm/opencl/bin:$PATH' \
   | sudo tee -a /etc/profile.d/rocm.sh
