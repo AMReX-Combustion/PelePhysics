456210141308a1aed41031a096b222f80f5e03de
Landon Owen <ldowen@sandia.gov>
2022-05-20 12:05:44 -0700

    Added LMeX spray case with variable jet and spray (or spread) angle, fixed bug in derived spray variables

--- PP_Spray/SprayDerive.cpp
+++ PP_Spray/SprayDerive.cpp
@@ -8,7 +8,7 @@ void
 SprayParticleContainer::computeDerivedVars(
   MultiFab& mf_var,
   const int level,
-  const int startComp,
+  const int start_indx,
   const Vector<std::string> derivePlotVars,
   std::string* sprayFuelNames)
 {
@@ -23,26 +23,33 @@ SprayParticleContainer::computeDerivedVars(
   const RealVect dx(AMREX_D_DECL(dxarr[0], dxarr[1], dxarr[2]));
   const RealVect plo(AMREX_D_DECL(ploarr[0], ploarr[1], ploarr[2]));
   const RealVect phi(AMREX_D_DECL(phiarr[0], phiarr[1], phiarr[2]));
-  int totalSpecComp = -1;
-  int avgSpecComp = -1;
+  const Real cell_vol = AMREX_D_TERM(dx[0], *dx[1], *dx[2]);
+  int total_spec_indx = -1;
   for (int ivar = 0; ivar < derivePlotVarCount; ++ivar) {
-    if (derivePlotVars[ivar] == "spray_total_mass_" + sprayFuelNames[0]) {
-      totalSpecComp = ivar;
-    } else if (derivePlotVars[ivar] == "spray_avg_mass_" + sprayFuelNames[0]) {
-      avgSpecComp = ivar;
+    if (derivePlotVars[ivar] == "spray_mass_" + sprayFuelNames[0]) {
+      total_spec_indx = ivar;
     }
   }
+  const int mass_indx = 0;
+  const int num_indx = mass_indx + 1;
+  const int vol_indx = num_indx + 1;
+  const int surf_indx = vol_indx + 1;
+  const int volf_indx = surf_indx + 1;
+  const int d10_indx = volf_indx + 1;
+  const int d32_indx = d10_indx + 1;
+  const int vel_indx = d32_indx + 1;
   const auto domain = this->Geom(level).Domain();
-  mf_var.setVal(0., startComp, derivePlotVarCount);
   for (MyParIter pti(*this, level); pti.isValid(); ++pti) {
     const Box tile_box = pti.tilebox();
     const Long Np = pti.numParticles();
     ParticleType* pstruct = &(pti.GetArrayOfStructs()[0]);
     const SprayData* fdat = d_sprayData;
-    Array4<Real> const& vararr = mf_var.array(pti, startComp);
+    FArrayBox& varfab = mf_var[pti];
+    Array4<Real> const& vararr = mf_var.array(pti, start_indx);
     amrex::ParallelFor(
-      Np, [pstruct, SPI, fdat, vararr, plo,
-           dxi] AMREX_GPU_DEVICE(int pid) noexcept {
+      Np, [pstruct, SPI, fdat, vararr, plo, dxi, cell_vol, mass_indx, num_indx,
+           vol_indx, surf_indx, volf_indx, d10_indx, d32_indx, vel_indx,
+           total_spec_indx] AMREX_GPU_DEVICE(int pid) noexcept {
         ParticleType& p = pstruct[pid];
         if (p.id() > 0) {
           RealVect lxc = (p.pos() - plo) * dxi;
@@ -54,49 +61,46 @@ SprayParticleContainer::computeDerivedVars(
             rho_part += p.rdata(SPI.pstateY + spf) / rhoL(*fdat, T_part, spf);
           }
           rho_part = 1. / rho_part;
-          Real pmass = M_PI / 6. * rho_part * std::pow(dia_part, 3);
-          Gpu::Atomic::Add(&vararr(ijkc, 0), pmass);
-          Gpu::Atomic::Add(&vararr(ijkc, 1), 1.);
-        }
-      });
-    amrex::ParallelFor(
-      Np, [pstruct, SPI, fdat, vararr, plo, dxi, totalSpecComp,
-           avgSpecComp] AMREX_GPU_DEVICE(int pid) noexcept {
-        ParticleType& p = pstruct[pid];
-        if (p.id() > 0) {
-          RealVect lxc = (p.pos() - plo) * dxi;
-          IntVect ijkc = lxc.floor(); // Cell with particle
-          Real cell_mass = vararr(ijkc, 0);
-          Real cell_num = vararr(ijkc, 1);
-          Real T_part = p.rdata(SPI.pstateT);
-          Real dia_part = p.rdata(SPI.pstateDia);
-          Real rho_part = 0.;
-          for (int spf = 0; spf < SPRAY_FUEL_NUM; ++spf) {
-            rho_part += p.rdata(SPI.pstateY + spf) / rhoL(*fdat, T_part, spf);
+          Real surf = M_PI * dia_part * dia_part;
+          Real vol = M_PI / 6. * std::pow(dia_part, 3);
+          Real pmass = vol * rho_part;
+          Real num_ppp = fdat->num_ppp; // Particles per parcel
+          Gpu::Atomic::Add(&vararr(ijkc, mass_indx), num_ppp * pmass);
+          Gpu::Atomic::Add(&vararr(ijkc, num_indx), num_ppp);
+          Gpu::Atomic::Add(&vararr(ijkc, vol_indx), num_ppp * vol);
+          Gpu::Atomic::Add(&vararr(ijkc, surf_indx), num_ppp * surf);
+          Gpu::Atomic::Add(&vararr(ijkc, volf_indx), num_ppp * vol / cell_vol);
+          Gpu::Atomic::Add(
+            &vararr(ijkc, d10_indx),
+            num_ppp * dia_part); // To be divided by num later
+          Gpu::Atomic::Add(
+            &vararr(ijkc, d32_indx),
+            num_ppp * vol / 6.); // To be divided by surf later
+          for (int dir = 0; dir < AMREX_SPACEDIM; ++dir) {
+            Gpu::Atomic::Add(
+              &vararr(ijkc, vel_indx + dir),
+              num_ppp * pmass * p.rdata(SPI.pstateVel + dir));
           }
-          rho_part = 1. / rho_part;
-          Real pmass = M_PI / 6. * rho_part * std::pow(dia_part, 3);
-          Real avg_temp = pmass * T_part / cell_mass;
-          Real avg_mass = pmass / cell_num;
-          Real avg_dia = pmass * dia_part / cell_mass;
-          Gpu::Atomic::Add(&vararr(ijkc, 2), avg_mass);
-          Gpu::Atomic::Add(&vararr(ijkc, 3), avg_dia);
-          Gpu::Atomic::Add(&vararr(ijkc, 4), avg_temp);
-          if (totalSpecComp >= 0) {
+          if (total_spec_indx >= 0) {
             for (int spf = 0; spf < SPRAY_FUEL_NUM; ++spf) {
               Gpu::Atomic::Add(
-                &vararr(ijkc, totalSpecComp + spf),
+                &vararr(ijkc, total_spec_indx + spf),
                 p.rdata(SPI.pstateY + spf) * pmass);
             }
           }
-          if (avgSpecComp >= 0) {
-            for (int spf = 0; spf < SPRAY_FUEL_NUM; ++spf) {
-              Gpu::Atomic::Add(
-                &vararr(ijkc, avgSpecComp + spf),
-                p.rdata(SPI.pstateY + spf) * pmass / cell_num);
-            }
-          }
         }
       });
+    varfab.protected_divide(
+      varfab, tile_box, tile_box, start_indx + num_indx, start_indx + d10_indx,
+      1); // Get d10
+    varfab.protected_divide(
+      varfab, tile_box, tile_box, start_indx + surf_indx, start_indx + d32_indx,
+      1); // Get d32
+    for (int dir = 0; dir < AMREX_SPACEDIM; ++dir) {
+      varfab.protected_divide(
+        varfab, tile_box, tile_box, start_indx + mass_indx,
+        start_indx + vel_indx + dir,
+        1); // Divide momentum by total mass
+    }
   }
 }
--- PP_Spray/SprayParticles.cpp
+++ PP_Spray/SprayParticles.cpp
@@ -168,29 +168,28 @@ SprayParticleContainer::readSprayParams(
 
   // List of known derived spray quantities
   std::vector<std::string> derive_name = {
-    "spray_total_mass", "spray_num_particles", "spray_avg_mass",
-    "spray_avg_dia", "spray_avg_temp"};
-  int plot_derive_vars = 1;
-  pp.query("plot_derive_vars", plot_derive_vars);
-  int plot_total_species = 0;
-  pp.query("plot_total_species", plot_total_species);
-  int plot_avg_species = 0;
-  pp.query("plot_avg_species", plot_avg_species);
+    "spray_mass",
+    "spray_num",
+    "spray_vol",
+    "spray_surf_area",
+    "spray_vol_frac",
+    "d10",
+    "d32",
+    AMREX_D_DECL("spray_x_vel", "spray_y_vel", "spray_z_vel")};
+  int derive_plot_vars = 1;
+  pp.query("derive_plot_vars", derive_plot_vars);
+  int derive_plot_species = 0;
+  pp.query("derive_plot_species", derive_plot_species);
   // If derive_spray_vars if present, add above spray quantities in the same
   // order
-  if (plot_derive_vars) {
+  if (derive_plot_vars) {
     for (int ivar = 0; ivar < derive_name.size(); ++ivar) {
       derivePlotVars.push_back(derive_name[ivar]);
     }
   }
-  if (plot_total_species) {
+  if (derive_plot_species && SPRAY_FUEL_NUM > 1) {
     for (int spf = 0; spf < SPRAY_FUEL_NUM; ++spf) {
-      derivePlotVars.push_back("spray_total_mass_" + sprayFuelNames[spf]);
-    }
-  }
-  if (plot_avg_species) {
-    for (int spf = 0; spf < SPRAY_FUEL_NUM; ++spf) {
-      derivePlotVars.push_back("spray_avg_mass_" + sprayFuelNames[spf]);
+      derivePlotVars.push_back("spray_mass_" + sprayFuelNames[spf]);
     }
   }
 
