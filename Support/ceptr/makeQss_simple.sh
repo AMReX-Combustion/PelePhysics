a9711952cfad0ccd7bade965185314e77d609ba3
Malik <malik.hassanaly@gmail.com>
2022-06-05 15:11:10 -0600

    Adding a simpler version of the qss mechanism which we can easily verify

---
+++ Support/ceptr/makeQss_simple.sh
@@ -0,0 +1,2 @@
+poetry run qssa -f ${PELE_PHYSICS_HOME}/Support/Mechanism/Models/dodecane_lu_qss_simple/skeletal.yaml -n ${PELE_PHYSICS_HOME}/Support/Mechanism/Models/dodecane_lu_qss_simple/non_qssa_list.yaml
+poetry run convert -f ${PELE_PHYSICS_HOME}/Support/Mechanism/Models/dodecane_lu_qss_simple/qssa.yaml
