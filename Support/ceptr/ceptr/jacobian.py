95f381d9103e12545b1c79ee97412f699ef62180
nickwimer <nicholas.wimer@gmail.com>
2022-06-13 13:09:12 -0700

    quick fix, undoing the QSS Jacobian parts

--- Support/ceptr/ceptr/jacobian.py
+++ Support/ceptr/ceptr/jacobian.py
@@ -819,8 +819,8 @@ def ajac_reaction_d(
         if not precond:
             cw.writer(fstream, "if (consP) {")
 
-            # for k in range(n_species):
-            for k in range(len(species_info.all_species_list)):
+            for k in range(n_species):
+            # for k in range(len(species_info.all_species_list)):
                 dqdc_s = denhancement_d(
                     mechanism, species_info, reaction, k, True
                 )
@@ -881,8 +881,8 @@ def ajac_reaction_d(
             cw.writer(fstream, "}")
             cw.writer(fstream, "else {")
 
-        # for k in range(n_species):
-        for k in range(len(species_info.all_species_list)):
+        for k in range(n_species):
+        # for k in range(len(species_info.all_species_list)):
             dqdc_s = denhancement_d(
                 mechanism, species_info, reaction, k, False
             )
@@ -945,8 +945,8 @@ def ajac_reaction_d(
 
         # print(reaction)
         # print(all_dict)
-        # for k in range(n_species):
-        for k in range(len(species_info.all_species_list)):
+        for k in range(n_species):
+        # for k in range(len(species_info.all_species_list)):
             dqdc_s = dqdc_d(
                 fstream,
                 mechanism,
