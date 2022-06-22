94a5ef9dd2893736b4d17359cb0faafa5135efab
Malik <malik.hassanaly@gmail.com>
2022-06-21 18:58:51 -0600

    remove useless copied file

--- Support/ceptr/makeQss_prof.sh
+++ Support/ceptr/makeQss_prof.sh
@@ -1,5 +1,7 @@
+cp ceptr/ceptr.py ceptr_main.py
 bash reenableAllProfile.sh
 rm ceptr_main.py.lprof
 
 poetry run qssa -f ${PELE_PHYSICS_HOME}/Support/Mechanism/Models/dodecane_lu_qss/skeletal.yaml -n ${PELE_PHYSICS_HOME}/Support/Mechanism/Models/dodecane_lu_qss/non_qssa_list.yaml
 kernprof -l  ceptr_main.py -f ${PELE_PHYSICS_HOME}/Support/Mechanism/Models/dodecane_lu_qss/qssa.yaml
+
