86b0f7439e8f042750a2abdc0fafffc0bc97b6c4
Malik <malik.hassanaly@gmail.com>
2022-07-07 10:59:26 -0600

    add a memory light version of production rate

--- Support/ceptr/ceptr/production.py
+++ Support/ceptr/ceptr/production.py
@@ -994,6 +994,476 @@ def production_rate(
 
     cw.writer(fstream)
 
+def production_rate_light(
+    fstream, mechanism, species_info, reaction_info
+):
+    """Write low memory production rate."""
+    n_species = species_info.n_species
+    n_reactions = mechanism.n_reactions
+
+    if len(reaction_info.index) != 7:
+        print("\n\nCheck this!!!\n")
+        sys.exit(1)
+
+    itroe = reaction_info.index[0:2]
+    isri = reaction_info.index[1:3]
+    ilindemann = reaction_info.index[2:4]
+    # i3body = reaction_info.index[3:5]
+    # isimple = reaction_info.index[4:6]
+    # ispecial = reaction_info.index[5:7]
+
+    ntroe = itroe[1] - itroe[0]
+    nsri = isri[1] - isri[0]
+    nlindemann = ilindemann[1] - ilindemann[0]
+    # n3body = i3body[1] - i3body[0]
+    # nsimple = isimple[1] - isimple[0]
+    # nspecial = ispecial[1] - ispecial[0]
+
+    # qdot
+    cw.writer(fstream)
+    cw.writer(
+        fstream,
+        cw.comment(
+            "GPU version of productionRate: no more use of thermo namespace"
+            " vectors "
+        ),
+    )
+    # main function
+    cw.writer(
+        fstream,
+        "AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void "
+        "productionRate_light("
+        "amrex::Real * wdot,"
+        "amrex::Real * sc," 
+        "amrex::Real * g_RT," 
+        "amrex::Real * g_RT_qss," 
+        "amrex::Real * sc_qss," 
+        "amrex::Real * kf_qss," 
+        "amrex::Real * qf_qss," 
+        "amrex::Real * qr_qss," 
+        "const amrex::Real * tc,"
+        "const amrex::Real invT)"
+    )
+    cw.writer(fstream, "{")
+
+    if n_reactions == 0:
+        cw.writer(fstream)
+    else:
+        cw.writer(
+            fstream,
+            cw.comment(
+                "reference concentration: P_atm / (RT) in inverse mol/m^3"
+            ),
+        )
+        cw.writer(
+            fstream,
+            "const amrex::Real refC = %g / %g * invT;"
+            % (
+                cc.Patm_pa,
+                cc.R.to(cc.ureg.joule / (cc.ureg.mole / cc.ureg.kelvin)).m,
+            ),
+        )
+        cw.writer(fstream, "const amrex::Real refCinv = 1 / refC;")
+
+        if nsri > 0:
+            cw.writer(fstream, "amrex::Real X, F_sri;")
+
+    cw.writer(fstream)
+    cw.writer(fstream, "for (int i = 0; i < %d; ++i) {" % n_species)
+    cw.writer(fstream, "wdot[i] = 0.0;")
+    cw.writer(fstream, "}")
+    cw.writer(fstream)
+
+    if n_reactions > 0:
+        # nclassd = n_reactions - nspecial
+        # nCorr   = n3body + ntroe + nsri + nlindemann
+
+        # Mixt concentration for PD & TB
+        cw.writer(fstream, cw.comment("compute the mixture concentration"))
+        cw.writer(fstream, "amrex::Real mixture = 0.0;")
+        cw.writer(fstream, "for (int i = 0; i < %d; ++i) {" % n_species)
+        cw.writer(fstream, "mixture += sc[i];")
+        cw.writer(fstream, "}")
+        cw.writer(fstream)
+
+        # Kc stuff
+        cw.writer(fstream, cw.comment("compute the Gibbs free energy"))
+        cw.writer(fstream, "gibbs(g_RT, tc);")
+        if species_info.n_qssa_species > 0:
+            cw.writer(fstream, "gibbs_qss(g_RT_qss, tc);")
+        cw.writer(fstream)
+
+        if species_info.n_qssa_species > 0:
+            cw.writer(fstream, cw.comment("Fill sc_qss here"))
+            cw.writer(fstream, "comp_k_f_qss(tc, invT, kf_qss);")
+            # cw.writer(fstream,"comp_Kc_qss(invT, g_RT, g_RT_qss, Kc_qss);")
+            cw.writer(
+                fstream,
+                "comp_qss_coeff(kf_qss, qf_qss, qr_qss, sc, tc, g_RT,"
+                " g_RT_qss);",
+            )
+            cw.writer(fstream, "comp_sc_qss(sc_qss, qf_qss, qr_qss);")
+            cw.writer(fstream)
+
+        # Loop like you're going through them in the mech.Linp order
+        for orig_idx, _ in reaction_info.idxmap.items():
+            reaction = mechanism.reaction(orig_idx)
+            cw.writer(fstream, "{")
+            if bool(reaction.orders):
+                forward_sc = cu.qss_sorted_phase_space(
+                    mechanism, species_info, reaction, reaction.orders
+                )
+            else:
+                forward_sc = cu.qss_sorted_phase_space(
+                    mechanism, species_info, reaction, reaction.reactants
+                )
+            if reaction.reversible:
+                reverse_sc = cu.qss_sorted_phase_space(
+                    mechanism, species_info, reaction, reaction.products
+                )
+            else:
+                reverse_sc = "0.0"
+
+            kc_exp_arg = cu.sorted_kc_exp_arg(
+                mechanism, species_info, reaction
+            )
+            kc_conv_inv = cu.fkc_conv_inv(
+                mechanism, species_info, reaction
+            )
+
+            dim = cu.phase_space_units(reaction.reactants)
+            third_body = reaction.reaction_type == "three-body"
+            falloff = reaction.reaction_type == "falloff"
+            is_troe = False
+            is_sri = False
+            aeuc = cu.activation_energy_units()
+            if not third_body and not falloff:
+                # Case 3 !PD, !TB
+                ctuc = cu.prefactor_units(cc.ureg("kmol/m**3"), 1 - dim)
+                pef = (
+                    reaction.rate.pre_exponential_factor * ctuc
+                ).to_base_units()
+                beta = reaction.rate.temperature_exponent
+                ae = (
+                    reaction.rate.activation_energy
+                    * cc.ureg.joule
+                    / cc.ureg.kmol
+                ).to(aeuc)
+            elif not falloff:
+                # Case 2 !PD, TB
+                ctuc = cu.prefactor_units(cc.ureg("kmol/m**3"), -dim)
+                pef = (
+                    reaction.rate.pre_exponential_factor * ctuc
+                ).to_base_units()
+                beta = reaction.rate.temperature_exponent
+                ae = (
+                    reaction.rate.activation_energy
+                    * cc.ureg.joule
+                    / cc.ureg.kmol
+                ).to(aeuc)
+            else:
+                # Case 1 PD, TB
+                ctuc = cu.prefactor_units(cc.ureg("kmol/m**3"), 1 - dim)
+                pef = (
+                    reaction.rate.high_rate.pre_exponential_factor * ctuc
+                ).to_base_units()
+                beta = reaction.rate.high_rate.temperature_exponent
+                ae = (
+                    reaction.rate.high_rate.activation_energy
+                    * cc.ureg.joule
+                    / cc.ureg.kmol
+                ).to(aeuc)
+
+                low_pef = (
+                    reaction.rate.low_rate.pre_exponential_factor * ctuc
+                ).to_base_units()
+                low_beta = reaction.rate.low_rate.temperature_exponent
+                low_ae = (
+                    reaction.rate.low_rate.activation_energy
+                    * cc.ureg.joule
+                    / cc.ureg.kmol
+                ).to(aeuc)
+                if reaction.rate.type == "Troe":
+                    troe = reaction.rate.falloff_coeffs
+                    ntroe = len(troe)
+                    is_troe = True
+                elif reaction.rate.type == "Sri":
+                    sri = reaction.rate.falloff_coeffs
+                    nsri = len(sri)
+                    # is_sri = True
+                elif reaction.rate.type == "Lindemann":
+                    pass
+                else:
+                    print(
+                        f"Unrecognized reaction rate type: {reaction.equation}"
+                    )
+                    sys.exit(1)
+
+            cw.writer(
+                fstream,
+                cw.comment("reaction %d:  %s" % (orig_idx, reaction.equation)),
+            )
+            cw.writer(fstream, "const amrex::Real k_f = %.15g" % (pef.m))
+
+            if (beta == 0) and (ae == 0):
+                cw.writer(fstream, "           ;")
+            else:
+                if ae == 0:
+                    cw.writer(
+                        fstream, "           * exp((%.15g) * tc[0]);" % (beta)
+                    )
+                elif beta == 0:
+                    cw.writer(
+                        fstream,
+                        "           * exp(-(%.15g) * invT);"
+                        % (((1.0 / cc.Rc / cc.ureg.kelvin)) * ae),
+                    )
+                else:
+                    cw.writer(
+                        fstream,
+                        "           * exp((%.15g) * tc[0] - (%.15g) * invT);"
+                        % (beta, (((1.0 / cc.Rc / cc.ureg.kelvin)) * ae)),
+                    )
+
+            alpha = None
+            if not third_body and not falloff:
+                cw.writer(
+                    fstream,
+                    "const amrex::Real qf = k_f * (%s);" % (forward_sc),
+                )
+            elif not falloff and len(reaction.efficiencies) == 1:
+                cw.writer(
+                    fstream,
+                    "const amrex::Real qf = k_f * (%s);" % (forward_sc),
+                )
+            elif not falloff:
+                alpha = enhancement_d_with_qss(
+                    mechanism, species_info, reaction
+                )
+                cw.writer(fstream, "const amrex::Real Corr = %s;" % (alpha))
+                cw.writer(
+                    fstream,
+                    "const amrex::Real qf = Corr * k_f * (%s);" % (forward_sc),
+                )
+            else:
+                alpha = enhancement_d_with_qss(
+                    mechanism, species_info, reaction
+                )
+                cw.writer(fstream, "amrex::Real Corr = %s;" % (alpha))
+                cw.writer(
+                    fstream,
+                    "const amrex::Real redP = Corr / k_f * %.15g "
+                    % (10 ** (-dim * 6) * low_pef.m * 10 ** (3**dim)),
+                )
+                cw.writer(
+                    fstream,
+                    "           * exp(%.15g * tc[0] - %.15g * invT);"
+                    % (low_beta, (1.0 / cc.Rc / cc.ureg.kelvin * low_ae)),
+                )
+                if is_troe:
+                    cw.writer(
+                        fstream, "const amrex::Real F = redP / (1.0 + redP);"
+                    )
+                    cw.writer(
+                        fstream, "const amrex::Real logPred = log10(redP);"
+                    )
+                    cw.writer(fstream, "const amrex::Real logFcent = log10(")
+                    if abs(troe[1]) > 1.0e-100:
+                        if 1.0 - troe[0] != 0:
+                            cw.writer(
+                                fstream,
+                                "    %.15g * exp(-tc[1] * %.15g)"
+                                % (1.0 - troe[0], (1 / troe[1])),
+                            )
+                    else:
+                        cw.writer(fstream, "     0.0 ")
+                    if abs(troe[2]) > 1.0e-100:
+                        if troe[0] != 0:
+                            cw.writer(
+                                fstream,
+                                "    + %.15g * exp(-tc[1] * %.15g)"
+                                % (troe[0], (1 / troe[2])),
+                            )
+                    else:
+                        cw.writer(fstream, "    + 0.0 ")
+                    if ntroe == 4:
+                        if troe[3] < 0:
+                            cw.writer(
+                                fstream, "    + exp(%.15g * invT));" % -troe[3]
+                            )
+                        else:
+                            cw.writer(
+                                fstream, "    + exp(-%.15g * invT));" % troe[3]
+                            )
+                    else:
+                        cw.writer(fstream, "    + 0.0);")
+                    cw.writer(
+                        fstream,
+                        "const amrex::Real troe_c = -0.4 - 0.67 * logFcent;",
+                    )
+                    cw.writer(
+                        fstream,
+                        "const amrex::Real troe_n = 0.75 - 1.27 * logFcent;",
+                    )
+                    cw.writer(
+                        fstream,
+                        "const amrex::Real troe = (troe_c + logPred) / (troe_n"
+                        " - 0.14 * (troe_c + logPred));",
+                    )
+                    cw.writer(
+                        fstream,
+                        "const amrex::Real F_troe = pow(10, logFcent / (1.0 +"
+                        " troe * troe));",
+                    )
+                    cw.writer(fstream, "Corr = F * F_troe;")
+                    cw.writer(
+                        fstream,
+                        "const amrex::Real qf = Corr * k_f * (%s);"
+                        % (forward_sc),
+                    )
+                elif is_sri:
+                    cw.writer(
+                        fstream, "const amrex::Real F = redP / (1.0 + redP);"
+                    )
+                    cw.writer(
+                        fstream, "const amrex::Real logPred = log10(redP);"
+                    )
+                    cw.writer(fstream, "X = 1.0 / (1.0 + logPred*logPred);")
+                    if sri[1] < 0:
+                        cw.writer(
+                            fstream,
+                            "F_sri = exp(X * log(%.15g * exp(%.15g * invT)"
+                            % (sri[0], -sri[1]),
+                        )
+                    else:
+                        cw.writer(
+                            fstream,
+                            "F_sri = exp(X * log(%.15g * exp(-%.15g * invT)"
+                            % (sri[0], sri[1]),
+                        )
+                    if sri[2] > 1.0e-100:
+                        cw.writer(
+                            fstream, "   +  exp(tc[0] / %.15g) " % sri[2]
+                        )
+                    else:
+                        cw.writer(fstream, "   +  0. ")
+                    cw.writer(
+                        fstream,
+                        "   *  (%d > 3 ? %.15g * exp(%.15g * tc[0]) : 1.0);"
+                        % (nsri, sri[3], sri[4]),
+                    )
+                    cw.writer(fstream, "Corr = F * F_sri;")
+                    cw.writer(
+                        fstream,
+                        "const amrex::Real qf = Corr * k_f * (%s);"
+                        % (forward_sc),
+                    )
+                elif nlindemann > 0:
+                    cw.writer(fstream, "Corr = redP / (1.0 + redP);")
+                    cw.writer(
+                        fstream,
+                        "const amrex::Real qf = Corr * k_f * (%s);"
+                        % (forward_sc),
+                    )
+            if kc_conv_inv:
+                if alpha is None:
+                    cw.writer(
+                        fstream,
+                        "const amrex::Real qr = k_f * exp(-(%s)) * (%s) *"
+                        " (%s);" % (kc_exp_arg, kc_conv_inv, reverse_sc),
+                    )
+                else:
+                    cw.writer(
+                        fstream,
+                        "const amrex::Real qr = Corr * k_f * exp(-(%s)) *"
+                        " (%s) * (%s);"
+                        % (kc_exp_arg, kc_conv_inv, reverse_sc),
+                    )
+            else:
+                if alpha is None:
+                    cw.writer(
+                        fstream,
+                        "const amrex::Real qr = k_f * exp(-(%s)) * (%s);"
+                        % (kc_exp_arg, reverse_sc),
+                    )
+                else:
+                    cw.writer(
+                        fstream,
+                        "const amrex::Real qr = Corr * k_f * exp(-(%s)) *"
+                        " (%s);" % (kc_exp_arg, reverse_sc),
+                    )
+            remove_forward = cu.is_remove_forward(reaction_info, orig_idx)
+
+            if remove_forward:
+                cw.writer(fstream, cw.comment("Remove forward reaction"))
+                cw.writer(
+                    fstream, cw.comment("const amrex::Real qdot = qf - qr;")
+                )
+                cw.writer(fstream, "const amrex::Real qdot = - qr;")
+            else:
+                cw.writer(fstream, "const amrex::Real qdot = qf - qr;")
+
+            reaction = mechanism.reaction(orig_idx)
+            lst_reactants = [(k, v) for k, v in reaction.reactants.items()]
+            lst_products = [(k, v) for k, v in reaction.products.items()]
+            all_agents = list(set(lst_reactants + lst_products))
+            agents = []
+            # remove QSS species from agents
+            for symbol, coefficient in all_agents:
+                if symbol not in species_info.qssa_species_list:
+                    agents.append((symbol, coefficient))
+            dict_species = {
+                v: i for i, v in enumerate(species_info.all_species_list)
+            }
+            agents = sorted(agents, key=lambda v: dict_species[v[0]])
+            # note that a species might appear as both reactant and product
+            # a species might also appear twice or more on on each side
+            # agents is a set that contains unique (symbol, coefficient)
+            for a in agents:
+                symbol, coefficient = a
+                for b in reaction.reactants:
+                    if b == a[0]:
+                        if coefficient == 1.0:
+                            cw.writer(
+                                fstream,
+                                "wdot[%d] -= qdot;"
+                                % (species_info.ordered_idx_map[symbol]),
+                            )
+                        else:
+                            cw.writer(
+                                fstream,
+                                "wdot[%d] -= %f * qdot;"
+                                % (
+                                    species_info.ordered_idx_map[symbol],
+                                    coefficient,
+                                ),
+                            )
+                for b in reaction.products:
+                    if b == a[0]:
+                        if coefficient == 1.0:
+                            cw.writer(
+                                fstream,
+                                "wdot[%d] += qdot;"
+                                % (species_info.ordered_idx_map[symbol]),
+                            )
+                        else:
+                            cw.writer(
+                                fstream,
+                                "wdot[%d] += %f * qdot;"
+                                % (
+                                    species_info.ordered_idx_map[symbol],
+                                    coefficient,
+                                ),
+                            )
+            cw.writer(fstream, "}")
+            cw.writer(fstream)
+
+    cw.writer(fstream)
+
+    cw.writer(fstream, "return;")
+    cw.writer(fstream, "}")
+
+    cw.writer(fstream)
 
 def production_rate_debug(
     fstream, mechanism, species_info, reaction_info, syms=None
