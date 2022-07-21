d3386ab26b4e67c5653b67e680581a8c78204a00
Malik <malik.hassanaly@gmail.com>
2022-07-21 14:11:56 -0600

    add better documentation

--- Support/ceptr/ceptr/ceptr.py
+++ Support/ceptr/ceptr/ceptr.py
@@ -61,9 +61,13 @@ def main():
         "-l", "--lst", help="Mechanism directory file list", type=str
     )
 
+    long_help = "Sytle format for .H file output."
+    long_help += "\nCPU: will print intermediate variables used for chainruling. This gives a readable version of the Jacobian entries, albeit memory consuming."
+    long_help += "\nGPU: will not print intermediate variables used for chainruling, and instead will replace them directly in the Jacobian entries."
+    long_help += " This gives a less readable version of the Jacobian, but more memory efficient."
     parser.add_argument(
         "--hformat",
-        help="Sytle format for .H file output",
+        help=long_help,
         type=str,
         choices=["cpu", "gpu"],
         default="gpu",
@@ -81,69 +85,87 @@ def main():
         "-rp",
         "--remove_pow",
         action="store_true",
-        help="Remove pow(...,n) if n=2,3 in printed expressions",
+        help="Replace pow(...,n) with multiplications or divisions if n<=3 and n>=-3 in printed expressions.",
     )
 
     parser.add_argument(
         "-rp10",
         "--remove_pow10",
         action="store_true",
-        help="Remove pow(10,...) in printed expressions",
+        help="Remove pow(10,x) in printed expressions and replace it with exp(ln(10)*x).",
     )
 
-    parser.add_argument(
-        "-roc",
-        "--gradual_op_count",
-        action="store_true",
-        help="Gradual elimination of expression (ensure monotonicity)",
-    )
-
-    parser.add_argument(
-        "-sj",
-        "--store_in_jacobian",
-        action="store_true",
-        help="Store temporary arrays in Jacobian array",
-    )
-
-    parser.add_argument(
-        "-rd",
-        "--round_decimals",
-        action="store_true",
-        help="Round decimal numbers when possible",
-    )
-
-    parser.add_argument(
-        "-rcse",
-        "--recycle_cse",
-        action="store_true",
-        help="Recycle common expressions when possible",
-    )
-
-    parser.add_argument(
-        "-rss",
-        "--remove_single_symbols_cse",
-        action="store_true",
-        help="Remove cse made of a single symbol",
-    )
-
+    long_help = "Counts number operations used to construct each common subexpression and replace"
+    long_help += " the common subexpression if the number of operations is less or equal to the value"
     parser.add_argument(
         "-moc",
         "--min_op_count",
         type=int,
         metavar="",
         required=False,
-        help="Min number of operation count per expression",
+        help=long_help,
         default=0,
     )
 
+    long_help = "Similar to --min_op_count but also counts how many times that common subexpression is used later."
+    long_help += " The meaning value passed is how many more operations will be done if the common subexpression is eliminated."
+    long_help += " This option only marginally increase the file size (therefore compile time), while still being memory efficient."
     parser.add_argument(
         "-moca",
         "--min_op_count_all",
         type=int,
         metavar="",
         required=False,
-        help="Min number of operation count saved per expression",
+        help=long_help,
         default=0,
+    )
+
+    long_help = "Gradual elimination of common subexpressions."
+    long_help += " Useful if --min_op_count or --min_op_count_all are active."
+    long_help += " Loops from 1 to the min_op_count and min_op_count_all values and gradually eliminate the common subexpressions."
+    long_help += " This has the advantage of ensuring that the memory footprint is strictly monotonically decreasing as min_op_count and min_op_count_all are increased."
+    parser.add_argument(
+        "-roc",
+        "--gradual_op_count",
+        action="store_true",
+        help=long_help,
+    )
+
+    long_help = "Use the Jacobian array as a temporary space to store intermediate variables."
+    long_help += " In particular, the last row of the Jacobian (dependence with respect to temperature) is done by finite difference which requires storing intermediate variables"
+    long_help += " (production rate, forward and backward reactions)."
+    long_help += " When the option is active, the `productionRate` function used to compute the finite difference"
+    long_help += " is replaced with a `productionRate_light` functions where references to different parts of the Jacobian are used in place of allocating new arrays."
+    parser.add_argument(
+        "-sj",
+        "--store_in_jacobian",
+        action="store_true",
+        help=long_help,
+    )
+
+    parser.add_argument(
+        "-rd",
+        "--round_decimals",
+        action="store_true",
+        help="Round decimal numbers when possible to minimize character count",
+    )
+
+    parser.add_argument(
+        "-rcse",
+        "--recycle_cse",
+        action="store_true",
+        help="Reuse common subexpressions that are not used later to avoid declaring new temporary reals",
+    )
+
+    long_help = "Remove common subexpressions that are made of 1 symbol."
+    long_help += " Those common subexpressions are typically `-xxx` and may not appear as worth replacing because they save 1 operations and are reused multiple times."
+    long_help += " However, when replaced in the later expressions, the `-` operations typically disappear or is merged into another"
+    long_help += " operations which actually does not increase the total number of operations."
+    parser.add_argument(
+        "-rss",
+        "--remove_single_symbols_cse",
+        action="store_true",
+        help=long_help,
     )
 
     args = parser.parse_args()
