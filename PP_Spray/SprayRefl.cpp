212d06d3bd4cdd3ce7027a6d8f7bdd26d9184705
Landon Owen <ldowen@sandia.gov>
2022-11-18 15:57:28 -0800

    Initialize time for splashed droplets

--- PP_Spray/SprayRefl.cpp
+++ PP_Spray/SprayRefl.cpp
@@ -113,6 +113,7 @@ SprayParticleContainer::CreateReflectedDroplets(
         for (int spf = 0; spf < SPRAY_FUEL_NUM; ++spf) {
           p.rdata(SPI.pstateY) = Y0[spf];
         }
+        p.rdata(SPI.pstateTime) = 0.;
         bool where = Where(p, pld);
         if (!where) {
           amrex::Abort("Bad reflected particle");
@@ -143,6 +144,7 @@ SprayParticleContainer::CreateReflectedDroplets(
         for (int spf = 0; spf < SPRAY_FUEL_NUM; ++spf) {
           p.rdata(SPI.pstateY) = Y0[spf];
         }
+        p.rdata(SPI.pstateTime) = 0.;
         bool where = Where(p, pld);
         if (!where) {
           amrex::Abort("Bad reflected particle");
