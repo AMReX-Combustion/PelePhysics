281547b728b4aa8c874dbf3ecb6d000c51957036
Jon Rood <jon.rood@nrel.gov>
2023-03-30 22:56:42 -0600

    Fix script.

--- Support/ceptr/gen.sh
+++ Support/ceptr/gen.sh
@@ -6,19 +6,16 @@ export PELE_PHYSICS_HOME=$(readlink -f ../..)
 MODEL_DIR=${PELE_PHYSICS_HOME}/Support/Mechanism/Models
 
 while read line; do
-  echo "${line}"
   poetry run convert -f ${MODEL_DIR}/${line} &
 done < ${MODEL_DIR}/list_mech
 wait
 
 while read line; do
-  echo "${line}"
   poetry run qssa -fq ${MODEL_DIR}/${line} &
 done < ${MODEL_DIR}/list_qss_mech
 wait
 
 while read line; do
-  echo "${line}"
   poetry run convert -fq ${MODEL_DIR}/${line} &
 done < ${MODEL_DIR}/list_qss_mech
 wait
