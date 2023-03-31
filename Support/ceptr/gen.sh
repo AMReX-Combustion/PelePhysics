2d89ac7629d320f15cce1030ff4eedffa61c7ccc
Jon Rood <jon.rood@nrel.gov>
2023-03-30 22:56:00 -0600

    Fix script.

--- Support/ceptr/gen.sh
+++ Support/ceptr/gen.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+# Generate mechanisms concurrently
+
 export PELE_PHYSICS_HOME=$(readlink -f ../..)
 MODEL_DIR=${PELE_PHYSICS_HOME}/Support/Mechanism/Models
 
@@ -9,14 +11,14 @@ while read line; do
 done < ${MODEL_DIR}/list_mech
 wait
 
-#while read line; do
-#  echo "${line}"
-#  poetry run qssa -fq ${MODEL_DIR}/${line} &
-#done < ${MODEL_DIR}/list_qss_mech
-#wait
-#
-#while read line; do
-#  echo "${line}"
-#  poetry run convert -fq ${MODEL_DIR}/${line} &
-#done < ${MODEL_DIR}/list_qss_mech
-#wait
+while read line; do
+  echo "${line}"
+  poetry run qssa -fq ${MODEL_DIR}/${line} &
+done < ${MODEL_DIR}/list_qss_mech
+wait
+
+while read line; do
+  echo "${line}"
+  poetry run convert -fq ${MODEL_DIR}/${line} &
+done < ${MODEL_DIR}/list_qss_mech
+wait
