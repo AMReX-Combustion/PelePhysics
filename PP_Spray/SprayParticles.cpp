19059a354ef04b03ecd3e6515e7ecb129cc7cabf
Landon Owen <ldowen@sandia.gov>
2023-05-03 17:03:59 -0700

    Significantly simplified and improved interpolation near EB and when small volume cells are encountered

--- PP_Spray/SprayParticles.cpp
+++ PP_Spray/SprayParticles.cpp
@@ -357,25 +357,6 @@ SprayParticleContainer::updateParticles(
           }
 #ifdef AMREX_USE_EB
           if (flags_array(cur_indx).isSingleValued()) {
-            if (volfrac_fab(cur_indx) < fdat->min_eb_vfrac) {
-              Real min_dis = 1.E12;
-              for (int aindx = 0; aindx < AMREX_D_PICK(2, 4, 8); ++aindx) {
-                IntVect cindx = indx_array[aindx];
-                if (volfrac_fab(cindx) > fdat->min_eb_vfrac) {
-                  Real dis = 0.;
-                  for (int dir = 0; dir < AMREX_SPACEDIM; ++dir) {
-                    dis += std::pow(
-                      p.pos(dir) - plo[dir] -
-                        static_cast<Real>(cindx[dir]) * dx[dir],
-                      2);
-                  }
-                  if (dis < min_dis) {
-                    min_dis = dis;
-                    cur_indx = cindx;
-                  }
-                }
-              }
-            }
             cvol *= 1. / (volfrac_fab(cur_indx));
           }
 #endif
