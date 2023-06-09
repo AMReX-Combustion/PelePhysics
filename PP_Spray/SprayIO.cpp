a10b336baad153b2caa433f8c47371a7cc915cef
Landon Owen <ldowen@sandia.gov>
2023-06-09 12:32:44 -0700

    Added total injected mass and time

--- PP_Spray/SprayIO.cpp
+++ PP_Spray/SprayIO.cpp
@@ -69,7 +69,7 @@ SprayParticleContainer::SprayParticleIO(
     }
     ParallelDescriptor::Barrier();
     // Each jet contains name, injection number density, outstanding mass and
-    // time, and minimum parcels for injection
+    // time, minimum parcels for injection, and total injected mass and time
     for (int jindx = 0; jindx < numjets; ++jindx) {
       SprayJet* js = m_sprayJets[jindx].get();
       int jet_proc = js->Proc();
@@ -82,9 +82,9 @@ SprayParticleContainer::SprayParticleIO(
         if (!file.good()) {
           FileOpenFailed(filename);
         }
-        file << js->jet_name() << " " << js->num_ppp() << " "
-             << js->m_sumInjMass << " " << js->m_sumInjTime << " "
-             << js->m_minParcel << "\n";
+        file << js->jet_name() << " " << js->m_sumInjMass << " "
+             << js->m_sumInjTime << " " << js->m_minParcel << " "
+             << js->m_totalInjMass << " " << js->m_totalInjTime << "\n";
         file.flush();
         file.close();
         if (!file.good()) {
@@ -126,9 +126,11 @@ SprayParticleContainer::PostInitRestart(const std::string& dir)
       Vector<Real> in_inj_mass(in_numjets);
       Vector<Real> in_inj_time(in_numjets);
       Vector<Real> in_min_parcel(in_numjets);
+      Vector<Real> in_total_mass(in_numjets);
+      Vector<Real> in_total_time(in_numjets);
       for (int i = 0; i < in_numjets; ++i) {
         JetDataFile >> in_jet_names[i] >> in_inj_mass[i] >> in_inj_time[i] >>
-          in_min_parcel[i];
+          in_min_parcel[i] >> in_total_mass >> in_total_time;
       }
       for (int ijets = 0; ijets < in_numjets; ++ijets) {
         std::string in_name = in_jet_names[ijets];
@@ -138,6 +140,8 @@ SprayParticleContainer::PostInitRestart(const std::string& dir)
             js->m_sumInjMass = in_inj_mass[ijets];
             js->m_sumInjTime = in_inj_time[ijets];
             js->m_minParcel = in_min_parcel[ijets];
+            js->m_totalInjMass = in_total_mass[ijets];
+            js->m_totalInjTime = in_total_time[ijets];
           }
         }
       }
