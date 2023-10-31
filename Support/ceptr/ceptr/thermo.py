c0b3d573ae0821b27f4e78216293080d69acf7d2
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2023-10-31 16:29:52 -0600

    Better symbolic handling of thermo models

--- Support/ceptr/ceptr/thermo.py
+++ Support/ceptr/ceptr/thermo.py
@@ -1,8 +1,6 @@
 """Thermodynamics functions."""
 
-import io
 import itertools
-from collections import OrderedDict
 
 import numpy as np
 
@@ -13,18 +11,18 @@ def thermo(fstream, mechanism, species_info, syms=None):
     """Write thermodynamics routines."""
     models = analyze_thermodynamics(mechanism, species_info.nonqssa_species_list)
     if species_info.n_qssa_species > 0:
-        qss_species_coeffs = analyze_thermodynamics_old(mechanism, species_info, 1)
+        qss_models = analyze_thermodynamics(mechanism, species_info.qssa_species_list)
 
     cv(fstream, species_info, models)
     cp(fstream, species_info, models)
     gibbs(fstream, species_info, models, 0, syms)
     if species_info.n_qssa_species > 0:
-        gibbs_old(fstream, species_info, qss_species_coeffs, 1, syms)
+        gibbs(fstream, species_info, qss_models, 1, syms)
     helmholtz(fstream, species_info, models)
     species_internal_energy(fstream, species_info, models)
     species_enthalpy(fstream, species_info, models, 0, syms)
     if species_info.n_qssa_species > 0:
-        species_enthalpy_old(fstream, species_info, qss_species_coeffs, 1, syms)
+        species_enthalpy(fstream, species_info, qss_models, 1, syms)
     species_entropy(fstream, species_info, models)
     dcvpdtemp(fstream, species_info, models)
 
@@ -106,30 +104,6 @@ def generator_attributes(name):
     return dct[name]
 
 
-def analyze_thermodynamics_old(mechanism, species_info, qss_flag):
-    """Extract information from the thermodynamics model."""
-    midpoints = OrderedDict()
-
-    spec_list = (
-        species_info.qssa_species_list
-        if qss_flag
-        else species_info.nonqssa_species_list
-    )
-    for symbol in spec_list:
-        species = mechanism.species(symbol)
-        model = species.thermo
-
-        assert model.n_coeffs == 15, "Unsupported thermo model."
-
-        mid = model.coeffs[0]
-        high_range = model.coeffs[1:8]
-        low_range = model.coeffs[8:15]
-
-        midpoints.setdefault(mid, []).append((species, low_range, high_range))
-
-    return midpoints
-
-
 def analyze_thermodynamics(mechanism, species_list):
     """Extract information from the thermodynamics model."""
     models = []
@@ -172,12 +146,6 @@ def generate_thermo_routine(
             f"AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void {name}(amrex::Real"
             " * species, const amrex::Real T)",
         )
-
-    # if syms:
-    #     syms_g_rt = name == "gibbs"
-    #     syms_g_rt_qss = name == "gibbs_qss"
-    #     syms_h_rt = name == "speciesEnthalpy"
-    #     syms_h_rt_qss = name == "speciesEnthalpy_qss"
 
     if not inline:
         cw.writer(fstream, "{")
@@ -234,7 +202,18 @@ def generate_thermo_routine(
                     fstream,
                     (f"result += y[{index}] * (" if inline else f"species[{index}] ="),
                 )
-                expression_generator(fstream, model["coefficients"][k])
+                if syms:
+                    models_smp = (
+                        syms.models_smp_tmp if not qss_flag else syms.models_qss_smp_tmp
+                    )
+                    model_smp = [
+                        x for x in models_smp if x["species"].name == species.name
+                    ][0]
+                    model_smp[name][k] = expression_generator(
+                        fstream, model["coefficients"][k], syms
+                    )
+                else:
+                    expression_generator(fstream, model["coefficients"][k])
                 if inline:
                     spec_idx = species_info.ordered_idx_map[species.name]
                     sp = species_info.nonqssa_species[spec_idx]
@@ -247,157 +226,6 @@ def generate_thermo_routine(
                     fstream,
                     "}",
                 )
-
-    if not inline:
-        cw.writer(fstream, "}")
-
-
-def generate_thermo_routine_old(
-    fstream,
-    species_info,
-    name,
-    expression_generator,
-    species_coeffs,
-    qss_flag,
-    needs_inv_temp=0,
-    needs_log_temp=False,
-    syms=None,
-    inline=False,
-):
-    """Write a thermodynamics routine."""
-    midpoints = species_coeffs
-
-    if not inline:
-        cw.writer(
-            fstream,
-            f"AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void {name}(amrex::Real"
-            " * species, const amrex::Real T)",
-        )
-
-    syms_g_rt = False
-    syms_g_rt_qss = False
-    syms_h_rt = False
-    syms_h_rt_qss = False
-    if name == "gibbs" and not (syms is None):
-        syms_g_rt = True
-    if name == "gibbs_qss" and not (syms is None):
-        syms_g_rt_qss = True
-    if name == "speciesEnthalpy" and not (syms is None):
-        syms_h_rt = True
-    if name == "speciesEnthalpy_qss" and not (syms is None):
-        syms_h_rt_qss = True
-
-    if not inline:
-        cw.writer(fstream, "{")
-
-    cw.writer(fstream, "const amrex::Real T2 = T*T;")
-    cw.writer(fstream, "const amrex::Real T3 = T*T*T;")
-    cw.writer(fstream, "const amrex::Real T4 = T*T*T*T;")
-    if needs_inv_temp != 0:
-        cw.writer(fstream, "const amrex::Real invT = 1.0 / T;")
-    if needs_inv_temp == 2:
-        cw.writer(fstream, "const amrex::Real invT2 = invT*invT;")
-    if needs_log_temp:
-        cw.writer(fstream, "const amrex::Real logT = log(T);")
-
-    for mid_temp, species_list in list(midpoints.items()):
-        lostream = io.StringIO()
-        for species, low_range, _ in species_list:
-            index = (
-                species_info.ordered_idx_map[species.name] - species_info.n_species
-                if qss_flag
-                else species_info.ordered_idx_map[species.name]
-            )
-            cw.writer(lostream, cw.comment(f"species {index}: {species.name}"))
-            cw.writer(
-                lostream,
-                (f"result += y[{index}] * (" if inline else f"species[{index}] ="),
-            )
-            if syms_g_rt:
-                syms.g_RT_smp_tmp[mid_temp]["m"][index] = expression_generator(
-                    lostream, low_range, syms
-                )
-            elif syms_g_rt_qss:
-                syms.g_RT_qss_smp_tmp[mid_temp]["m"][index] = expression_generator(
-                    lostream, low_range, syms
-                )
-            elif syms_h_rt:
-                syms.h_RT_smp_tmp[mid_temp]["m"][index] = expression_generator(
-                    lostream, low_range, syms
-                )
-            elif syms_h_rt_qss:
-                syms.h_RT_qss_smp_tmp[mid_temp]["m"][index] = expression_generator(
-                    lostream, low_range, syms
-                )
-            else:
-                expression_generator(lostream, low_range)
-            if inline:
-                spec_idx = species_info.ordered_idx_map[species.name]
-                sp = species_info.nonqssa_species[spec_idx]
-                imw = 1.0 / sp.weight
-                cw.writer(lostream, f")* {imw:.16f}")
-            cw.writer(lostream, ";")
-
-        histream = io.StringIO()
-        for species, _, high_range in species_list:
-            index = (
-                species_info.ordered_idx_map[species.name] - species_info.n_species
-                if qss_flag
-                else species_info.ordered_idx_map[species.name]
-            )
-            cw.writer(
-                histream,
-                cw.comment(f"species {index}: {species.name}"),
-            )
-            cw.writer(
-                histream,
-                (f"result += y[{index}] * (" if inline else f"species[{index}] ="),
-            )
-            if syms_g_rt:
-                syms.g_RT_smp_tmp[mid_temp]["p"][index] = expression_generator(
-                    histream, high_range, syms
-                )
-            elif syms_g_rt_qss:
-                syms.g_RT_qss_smp_tmp[mid_temp]["p"][index] = expression_generator(
-                    histream, high_range, syms
-                )
-            elif syms_h_rt:
-                syms.h_RT_smp_tmp[mid_temp]["p"][index] = expression_generator(
-                    histream, high_range, syms
-                )
-            elif syms_h_rt_qss:
-                syms.h_RT_qss_smp_tmp[mid_temp]["p"][index] = expression_generator(
-                    histream, high_range, syms
-                )
-            else:
-                expression_generator(histream, high_range)
-            if inline:
-                spec_idx = species_info.ordered_idx_map[species.name]
-                sp = species_info.nonqssa_species[spec_idx]
-                imw = 1.0 / sp.weight
-                cw.writer(histream, f")* {imw:.16f}")
-            cw.writer(histream, ";")
-
-        lostr = lostream.getvalue().rstrip("\n")
-        histr = histream.getvalue().rstrip("\n")
-        cw.writer(fstream, "")
-        if histr == lostr:
-            cw.writer(
-                fstream,
-                cw.comment("species with no change at a midpoint T"),
-            )
-            cw.writer(fstream, lostr)
-        else:
-            cw.writer(
-                fstream,
-                cw.comment(f"species with midpoint at T={mid_temp:g} kelvin"),
-            )
-            cw.writer(
-                fstream,
-                f"""if (T < {mid_temp:g}) {{\n{lostr}}} else {{\n{histr}}}""",
-            )
-        lostream.close()
-        histream.close()
 
     if not inline:
         cw.writer(fstream, "}")
@@ -423,24 +251,6 @@ def gibbs(fstream, species_info, models, qss_flag, syms=None):
     cw.writer(fstream)
     cw.writer(fstream, cw.comment("compute the g/(RT) at the given temperature"))
     generate_thermo_routine(fstream, species_info, name, models, qss_flag, syms)
-
-
-def gibbs_old(fstream, species_info, species_coeffs, qss_flag, syms=None):
-    """Write Gibbs."""
-    name = "gibbs_qss" if qss_flag else "gibbs"
-    cw.writer(fstream)
-    cw.writer(fstream, cw.comment("compute the g/(RT) at the given temperature"))
-    generate_thermo_routine_old(
-        fstream,
-        species_info,
-        name,
-        gibbs_nasa7,
-        species_coeffs,
-        qss_flag,
-        1,
-        True,
-        syms,
-    )
 
 
 def helmholtz(fstream, species_info, models):
@@ -484,28 +294,6 @@ def species_enthalpy(fstream, species_info, models, qss_flag, syms=None):
         name,
         models,
         qss_flag,
-        syms,
-    )
-
-
-def species_enthalpy_old(fstream, species_info, species_coeffs, qss_flag, syms=None):
-    """Write species enthalpy."""
-    name = "speciesEnthalpy_qss" if qss_flag else "speciesEnthalpy"
-    cw.writer(fstream)
-    cw.writer(
-        fstream,
-        cw.comment("compute the h/(RT) at the given temperature (Eq 20)"),
-    )
-
-    generate_thermo_routine_old(
-        fstream,
-        species_info,
-        name,
-        enthalpy_nasa7,
-        species_coeffs,
-        qss_flag,
-        1,
-        False,
         syms,
     )
 
