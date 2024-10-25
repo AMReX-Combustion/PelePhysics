156b36a988d8f6f4519cc2523400b4ea7712aad6
Bruce Perry <bruce.perry@nrel.gov>
2024-10-25 15:29:22 -0600

    simplify templated functions in EOS.cpp

--- Source/Eos/EOS.cpp
+++ Source/Eos/EOS.cpp
@@ -6,51 +6,6 @@ template <typename EOSType>
 void
 atomic_weightsCHON(amrex::Real* /*atwCHON*/)
 {
-}
-
-template <typename EOSType>
-void
-element_compositionCHON(int* /*ecompCHON*/)
-{
-}
-
-template <typename EOSType>
-void
-speciesNames(amrex::Vector<std::string>& /*spn*/)
-{
-}
-
-template <>
-void
-atomic_weightsCHON<GammaLaw>(amrex::Real atwCHON[])
-{
-  // CHON
-  for (int i = 0; i < 4; i++) {
-    atwCHON[i] = 0.0;
-  }
-}
-
-template <>
-void
-element_compositionCHON<GammaLaw>(int ecompCHON[])
-{
-  for (int k = 0; k < 4; k++) {
-    ecompCHON[k] = 0;
-  }
-}
-
-template <>
-void
-speciesNames<GammaLaw>(amrex::Vector<std::string>& spn)
-{
-  spn.resize(1);
-  spn[0] = "AIR";
-}
-
-template <>
-void
-atomic_weightsCHON<Fuego>(amrex::Real atwCHON[])
-{
   amrex::Vector<std::string> ename;
   CKSYME_STR(ename);
   amrex::Real atw[NUM_ELEMENTS];
@@ -75,9 +30,9 @@ atomic_weightsCHON<Fuego>(amrex::Real atwCHON[])
   }
 }
 
-template <>
+template <typename EOSType>
 void
-element_compositionCHON<Fuego>(int ecompCHON[])
+element_compositionCHON(int* /*ecompCHON*/)
 {
   amrex::Vector<std::string> ename;
   CKSYME_STR(ename);
@@ -110,32 +65,38 @@ element_compositionCHON<Fuego>(int ecompCHON[])
   }
 }
 
-template <>
+template <typename EOSType>
 void
-speciesNames<Fuego>(amrex::Vector<std::string>& spn)
+speciesNames(amrex::Vector<std::string>& /*spn*/)
 {
   CKSYMS_STR(spn);
 }
 
 template <>
 void
-atomic_weightsCHON<SRK>(amrex::Real atwCHON[])
+atomic_weightsCHON<GammaLaw>(amrex::Real atwCHON[])
 {
-  atomic_weightsCHON<Fuego>(atwCHON);
+  // CHON
+  for (int i = 0; i < 4; i++) {
+    atwCHON[i] = 0.0;
+  }
 }
 
 template <>
 void
-element_compositionCHON<SRK>(int ecompCHON[])
+element_compositionCHON<GammaLaw>(int ecompCHON[])
 {
-  element_compositionCHON<Fuego>(ecompCHON);
+  for (int k = 0; k < 4; k++) {
+    ecompCHON[k] = 0;
+  }
 }
 
 template <>
 void
-speciesNames<SRK>(amrex::Vector<std::string>& spn)
+speciesNames<GammaLaw>(amrex::Vector<std::string>& spn)
 {
-  speciesNames<Fuego>(spn);
+  spn.resize(1);
+  spn[0] = "AIR";
 }
 
 template <>
