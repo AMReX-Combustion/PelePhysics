3bea24cc2fd7dd4e459af195de82232d8662335b
Bruce Perry <bruce.perry@nrel.gov>
2024-12-11 17:25:22 -0700

    update sundials - sycl 2025 fix

--- .github/workflows/dependencies/dependencies_dpcpp.sh
+++ .github/workflows/dependencies/dependencies_dpcpp.sh
@@ -6,4 +6,4 @@ sudo wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUC
 sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
 sudo apt-get update
-sudo apt-get install -y intel-oneapi-compiler-dpcpp-cpp=2024.2.1-1079 intel-oneapi-mkl-devel=2024.2.2-15
+sudo apt-get install -y intel-oneapi-compiler-dpcpp-cpp intel-oneapi-mkl-devel
