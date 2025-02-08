9eacf47e3734f6079779630e88712b6908e00d8a
Justin Freiberger <justin.freiberger@rwth-aachen.de>
2025-02-08 19:04:19 +0100

    changed the iteratios through dbin and calculations of Ddiag

--- Support/ceptr/ceptr/transport.py
+++ Support/ceptr/ceptr/transport.py
@@ -572,14 +572,14 @@ def diffcoefs(fstream, species_info, species_transport, ntfit):
     cw.writer(fstream, "AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE")
     cw.writer(fstream, "void egtransetCOFD(amrex::Real* COFD) {")
 
-    #only changed stuff herer, talk bout that and del this com
+    # map the upper Matrix to the one dimensional Array
     for i, _ in enumerate(spec_ordered):
         for j, _ in enumerate(spec_ordered[0:i+1]):
             for k in range(4):
                 cw.writer(
-                    NUM_SPECIES * i - i * (i + 1)/2 + j
+                    fstream,
                     f"{'COFD'}[{int(4 * (i*(i + 1)/2 + j) + k)}]"
-                    f" = {cofd[j][i][3 - k]:.8E};",
+                    f" = {cofd[i][j][3 - k]:.8E};",
                 )
 
     cw.writer(fstream, "}")
