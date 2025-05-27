04cb9c79d28abaaec312397e43c905daf9581c78
Bruce Perry <bruce.perry@nrel.gov>
2025-05-27 17:54:04 -0600

    new better TurbFileHIT with genfromplt, genfromplanes

--- Support/TurbFileHIT/main.cpp
+++ Support/TurbFileHIT/main.cpp
@@ -1,148 +1,30 @@
-#include <iostream>
-
-#include "AMReX_ParmParse.H"
-#include <AMReX_ParallelDescriptor.H>
-#include <HITData.H>
-#include <main_K.H>
+#include "main.H"
 
 using namespace amrex;
 
 static void
-Extend(FArrayBox& xfab, FArrayBox& vfab, const Box& domain)
+print_usage(int, char* argv[])
 {
-  Box tbx = vfab.box();
-
-  tbx.setBig(0, domain.bigEnd(0) + 3);
-
-  const int ygrow = AMREX_SPACEDIM == 3 ? 3 : 1;
-
-  tbx.setBig(1, domain.bigEnd(1) + ygrow);
-
-  xfab.resize(tbx, 1);
-
-  Box orig_box = vfab.box();
-  vfab.shift(0, 1);
-  vfab.shift(1, 1);
-  xfab.copy(vfab); // (0,0)
-
-  vfab.shift(0, domain.length(0)); // (1,0)
-  xfab.copy(vfab);
-  vfab.shift(1, domain.length(1)); // (1,1)
-  xfab.copy(vfab);
-  vfab.shift(0, -domain.length(0)); // (0,1)
-  xfab.copy(vfab);
-  vfab.shift(0, -domain.length(0)); // (-1,1)
-  xfab.copy(vfab);
-  vfab.shift(1, -domain.length(1)); // (-1,0)
-  xfab.copy(vfab);
-  vfab.shift(1, -domain.length(1)); // (-1,-1)
-  xfab.copy(vfab);
-  vfab.shift(0, domain.length(0)); // (0,-1)
-  xfab.copy(vfab);
-  vfab.shift(0, domain.length(0)); // (1,-1)
-  xfab.copy(vfab);
-  vfab.shift(0, -domain.length(0) - 1);
-  vfab.shift(1, domain.length(1) - 1);
-
-  if (vfab.box() != orig_box)
-    Abort("Oops, something bad happened");
-}
-
-void
-readHIT(HITData* a_data)
-{
-  ParmParse pp;
-  std::string hit_file("IC");
-  pp.query("hit_file", hit_file);
-
-  pp.query("input_ncell", a_data->input_ncell);
-  if (a_data->input_ncell == 0) {
-    amrex::Abort("for HIT, the input ncell cannot be 0 !");
-  }
-
-  int binfmt = 0; // Default is ASCII format
-  pp.query("input_binaryformat", binfmt);
-  pp.query("urms0", a_data->urms0);
-
-  amrex::Real lambda0 = 0.5;
-  amrex::Real tau = lambda0 / a_data->urms0;
-
-  // Output initial conditions
-  std::ofstream ofs("initialConditions.txt", std::ofstream::out);
-  amrex::Print(ofs) << "lambda0, urms0, tau \n";
-  amrex::Print(ofs).SetPrecision(17)
-    << lambda0 << "," << a_data->urms0 << "," << tau << std::endl;
-  ofs.close();
-
-  // Read initial velocity field
-  const size_t nx = a_data->input_ncell;
-  const size_t ny = a_data->input_ncell;
-  const size_t nz = a_data->input_ncell;
-  amrex::Vector<amrex::Real> data(
-    nx * ny * nz * 6); /* this needs to be double */
-  if (binfmt) {
-    read_binary(hit_file, nx, ny, nz, 6, data);
-  } else {
-    read_csv(hit_file, nx, ny, nz, data);
-  }
-
-  // Extract position and velocities
-  amrex::Vector<amrex::Real> xinput;
-  amrex::Vector<amrex::Real> uinput;
-  amrex::Vector<amrex::Real> vinput;
-  amrex::Vector<amrex::Real> winput;
-  amrex::Vector<amrex::Real> xdiff;
-  amrex::Vector<amrex::Real> xarray;
-
-  xinput.resize(nx * ny * nz);
-  uinput.resize(nx * ny * nz);
-  vinput.resize(nx * ny * nz);
-  winput.resize(nx * ny * nz);
-
-  for (long i = 0; i < xinput.size(); i++) {
-    xinput[i] = data[0 + i * 6];
-    uinput[i] = data[3 + i * 6] * a_data->urms0 / a_data->uin_norm;
-    vinput[i] = data[4 + i * 6] * a_data->urms0 / a_data->uin_norm;
-    winput[i] = data[5 + i * 6] * a_data->urms0 / a_data->uin_norm;
-  }
-
-  // Get the xarray table and the differences.
-  xarray.resize(nx);
-  for (long i = 0; i < xarray.size(); i++) {
-    xarray[i] = xinput[i];
-  }
-  xdiff.resize(nx);
-  std::adjacent_difference(xarray.begin(), xarray.end(), xdiff.begin());
-  xdiff[0] = xdiff[1];
-
-  // Make sure the search array is increasing
-  if (not std::is_sorted(xarray.begin(), xarray.end())) {
-    amrex::Abort("Error: non ascending x-coordinate array.");
-  }
-
-  // Pass data to the prob_parm
-  a_data->Linput = xarray[nx - 1] + 0.5 * xdiff[nx - 1];
-
-  a_data->d_xarray =
-    (amrex::Real*)amrex::The_Arena()->alloc(nx * sizeof(amrex::Real));
-  a_data->d_xdiff =
-    (amrex::Real*)amrex::The_Arena()->alloc(nx * sizeof(amrex::Real));
-  a_data->d_uinput =
-    (amrex::Real*)amrex::The_Arena()->alloc(nx * ny * nz * sizeof(amrex::Real));
-  a_data->d_vinput =
-    (amrex::Real*)amrex::The_Arena()->alloc(nx * ny * nz * sizeof(amrex::Real));
-  a_data->d_winput =
-    (amrex::Real*)amrex::The_Arena()->alloc(nx * ny * nz * sizeof(amrex::Real));
-
-  for (int i = 0; i < nx; i++) {
-    a_data->d_xarray[i] = xarray[i];
-    a_data->d_xdiff[i] = xdiff[i];
-  }
-  for (int i = 0; i < nx * ny * nz; i++) {
-    a_data->d_uinput[i] = uinput[i];
-    a_data->d_vinput[i] = vinput[i];
-    a_data->d_winput[i] = winput[i];
-  }
+  std::cerr << "usage:\n";
+  std::cerr << argv[0]
+            << " type=<turb_box, periodic_plt, diag_frame_planes>"
+               " ofile=<output directory name>\n";
+  std::cerr << "   Additional inputs for type=diag_frame_planes:\n";
+  std::cerr << "      ifiles=<list of DiagFramePlanes to include>\n";
+  std::cerr << "      level=<AMR level from DiagFramePlanes to use, DEF=0>\n";
+  std::cerr << "      periodicity=<of input planes, DEF='1 1 1'>\n";
+  std::cerr << "      normal=<normal dir for input DiagFramePlanes>\n";
+  std::cerr << "   Additional inputs for type=periodic_plt:\n";
+  std::cerr << "      ifile=<Plt file, must be per in norm direction>\n";
+  std::cerr << "      normal=<normal direction for planes being extracted>\n";
+  std::cerr << "      level=<AMR level from Plot file to use, DEF=0>\n";
+  std::cerr << "      periodicity=<periodicity of Plot file, DEF='1 1 1'>\n";
+  std::cerr << "   Additional inputs for type=turb_box:\n";
+  std::cerr << "      hit_file=<cubic isotropic turbulence file to use>\n";
+  std::cerr << "      input_ncell=<ncells in each direction in turb_file>\n";
+  std::cerr << "      input_binaryformat=<infile is binary not ASCII>\n";
+  std::cerr << "      urms0=<desired velocity fluctuation scale>\n";
+  exit(1);
 }
 
 int
@@ -150,40 +32,18 @@ main(int argc, char* argv[])
 {
   Initialize(argc, argv);
   {
+    if (argc < 2)
+      print_usage(argc, argv);
+
     ParmParse pp;
 
-    HITData data;
+    const std::string farg = amrex::get_command_argument(1);
+    if (farg == "-h" || farg == "--help")
+      print_usage(argc, argv);
 
-    readHIT(&data);
-
-    int ncell = data.input_ncell;
-    Real xlo = data.d_xarray[0];
-    Real xhi = data.d_xarray[ncell - 1];
-
-    Box box_turb(
-      IntVect(AMREX_D_DECL(0, 0, 0)),
-      IntVect(AMREX_D_DECL(ncell - 1, ncell - 1, ncell - 1)));
-    RealBox rb_turb(
-      {AMREX_D_DECL(xlo, xlo, xlo)}, {AMREX_D_DECL(xhi, xhi, xhi)});
-    int coord_turb(0);
-    Array<int, AMREX_SPACEDIM> per_turb = {AMREX_D_DECL(1, 1, 1)};
-    Geometry geom_turb(box_turb, rb_turb, coord_turb, per_turb);
-    const Real* dx_turb = geom_turb.CellSize();
-
-    // Fill the velocity FAB with HIT
-    FArrayBox vel_turb(box_turb, AMREX_SPACEDIM);
-    Array4<Real> const& fab = vel_turb.array();
-    auto geomdata = geom_turb.data();
-    AMREX_PARALLEL_FOR_3D(
-      box_turb, i, j, k, { fillVelFab(i, j, k, fab, geomdata, data); });
-
-    std::ofstream fabOut;
-    fabOut.open("Turb_D", std::ios::out | std::ios::trunc);
-    vel_turb.writeOn(fabOut);
-    // Write turbulence file for TurbInflow
-    std::string TurbDir("Turb");
-    pp.query("TurbFile", TurbDir);
-
+    std::string ofile;
+    pp.get("ofile", ofile);
+    std::string TurbDir = ofile;
     if (ParallelDescriptor::IOProcessor())
       if (!UtilCreateDirectory(TurbDir, 0755))
         CreateDirectoryFailed(TurbDir);
@@ -203,60 +63,15 @@ main(int argc, char* argv[])
     if (!ifsd.good())
       FileOpenFailed(Dat);
 
-    //
-    // Write the first part of the Turb header.
-    // Note that this is solely for periodic style inflow files.
-    //
-    Box box_turb_io(box_turb);
-    box_turb_io.setBig(0, box_turb.bigEnd(0) + 3);
-    box_turb_io.setBig(1, box_turb.bigEnd(1) + 3);
-    box_turb_io.setBig(2, box_turb.bigEnd(2) + 1);
-
-    ifsh << box_turb_io.length(0) << ' ' << box_turb_io.length(1) << ' '
-         << box_turb_io.length(2) << '\n';
-
-    ifsh << rb_turb.length(0) + 2 * dx_turb[0] << ' '
-         << rb_turb.length(1) + 2 * dx_turb[1] << ' ' << rb_turb.length(2)
-         << '\n';
-
-    ifsh << per_turb[0] << ' ' << per_turb[1] << ' ' << per_turb[2] << '\n';
-
-    // Dump field as a "turbulence file"
-    IntVect sm = box_turb.smallEnd();
-    IntVect bg = box_turb.bigEnd();
-    int dir = AMREX_SPACEDIM - 1;
-    FArrayBox xfab, TMP;
-    //
-    // We work on one cell wide Z-planes.
-    // We first do the lo AMREX_SPACEDIM plane.
-    // And then all the other planes in xhi -> xlo order.
-    //
-    for (int d = 0; d < AMREX_SPACEDIM; ++d) {
-      bg[dir] = sm[dir];
-      {
-        Box bx(sm, bg);
-        TMP.resize(bx, 1);
-        TMP.copy(vel_turb, bx, d, bx, 0, 1);
-        Extend(xfab, TMP, box_turb);
-        ifsh << ifsd.tellp() << std::endl;
-        xfab.writeOn(ifsd);
-      }
-      for (int i = box_turb.bigEnd(dir); i >= box_turb.smallEnd(dir); i--) {
-        sm[dir] = i;
-        bg[dir] = i;
-        Box bx(sm, bg);
-        TMP.resize(bx, 1);
-        TMP.copy(vel_turb, bx, d, bx, 0, 1);
-        Extend(xfab, TMP, box_turb);
-        ifsh << ifsd.tellp() << std::endl;
-        xfab.writeOn(ifsd);
-      }
+    InputType itype;
+    pp.get("type", itype);
+    if (itype == InputType::diag_frame_planes) {
+      turbinflow_from_diag_frame_planes(ifsd, ifsh);
+    } else if (itype == InputType::periodic_plt) {
+      turbinflow_from_periodic_plt(ifsd, ifsh);
+    } else {
+      turbinflow_from_turb_box(ifsd, ifsh);
     }
-    amrex::The_Arena()->free(data.d_xarray);
-    amrex::The_Arena()->free(data.d_xdiff);
-    amrex::The_Arena()->free(data.d_uinput);
-    amrex::The_Arena()->free(data.d_vinput);
-    amrex::The_Arena()->free(data.d_winput);
   }
   Finalize();
 }
