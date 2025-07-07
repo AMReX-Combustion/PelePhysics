a077e7c90b8ae99e2a66c19e0a36ea6d5deff0f2
efhunt <63171168+efhunt@users.noreply.github.com>
2025-07-07 11:28:35 +0100

    formatting

--- Source/Utility/TurbForcing/TurbForcing.cpp
+++ Source/Utility/TurbForcing/TurbForcing.cpp
@@ -729,7 +729,8 @@ TurbForcing::addTurbVelForces(
 
   // Need all of ffarr filled for next lambda
   amrex::Gpu::synchronize();
- 
+
+  
   // Now interpolate onto fine grid
   if (a_incompressible == 0) {
     amrex::ParallelFor(
