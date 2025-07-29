cd5e8943cb09cac6ede4e077c825aedf8ace0744
dmontgomeryNREL <David.Montgomery@nrel.gov>
2025-07-29 15:42:25 -0600

    Make calculation of diff between liquid and gas phase enthalpy more explicit.

--- Source/Spray/SpraySetup.cpp
+++ Source/Spray/SpraySetup.cpp
@@ -79,7 +79,6 @@ SprayParticleContainer::readSprayParams(int& particle_verbose)
       } else {
         m_sprayDepNames[i] = m_sprayFuelNames[i];
       }
-      m_sprayData->liqprops.latent[i] = m_sprayData->liqprops.ref_latent[i];
     }
   }
 
@@ -242,7 +241,7 @@ SprayParticleContainer::spraySetup(const Real* body_force)
   eos.T2Hi(m_sprayData->liqprops.ref_T, fuelEnth.data());
   for (int ns = 0; ns < SPRAY_FUEL_NUM; ++ns) {
     const int fspec = m_sprayData->indx[ns];
-    m_sprayData->liqprops.latent[ns] -= fuelEnth[fspec] * SPU.eng_conv;
+    m_sprayData->liqprops.diff_liq_gas_latent[ns] = m_sprayData->liqprops.latent[ns] - fuelEnth[fspec] * SPU.eng_conv;
   }
   for (int dir = 0; dir < AMREX_SPACEDIM; ++dir) {
     m_sprayData->body_force[dir] = body_force[dir];
