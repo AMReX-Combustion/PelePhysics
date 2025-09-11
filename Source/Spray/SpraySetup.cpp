b21b9864a6e49cd282b2109bbb3f097ed6206343
SreejithNREL <Sreejith.NadakkalAppukuttan@nrel.gov>
2025-09-10 21:15:27 -0600

    Added functionality to read species molecular weights from a metadata file for manifold models

--- Source/Spray/SpraySetup.cpp
+++ Source/Spray/SpraySetup.cpp
@@ -238,6 +238,24 @@ SprayParticleContainer::spraySetup(
   m_sprayData->indx[0] = 0;
   m_sprayData->dep_indx[0] = 0;
 #endif
+  // I think the below code snippet,as they are, should be applicable to
+  // non-manifold models only.
+  SprayUnits SPU;
+
+  Vector<Real> fuelEnth(NUM_SPECIES);
+  auto eos = pele::physics::PhysicsType::eos(eosparms_h);
+  amrex::GpuArray<amrex::Real, NUM_SPECIES> mw;
+  eos.molecular_weight(mw.data());
+  m_sprayData->liqprops.init_mw(mw, m_sprayData->indx.data());
+  eos.T2Hi(m_sprayData->liqprops.ref_T, fuelEnth.data());
+  for (int ns = 0; ns < SPRAY_FUEL_NUM; ++ns) {
+    const int fspec = m_sprayData->indx[ns];
+    m_sprayData->liqprops.latentRef_minus_gasRefH_i[ns] =
+      m_sprayData->liqprops.latent[ns] - fuelEnth[fspec] * SprayUnits::eng_conv;
+  }
+  for (int dir = 0; dir < AMREX_SPACEDIM; ++dir) {
+    m_sprayData->body_force[dir] = body_force[dir];
+  }
 #endif
 
 #ifdef USE_MANIFOLD_EOS
@@ -263,24 +281,29 @@ SprayParticleContainer::spraySetup(
       Abort("Fuel " + m_sprayDepNames[i] + " not found in species list");
     }
   }
+
+  /*
+   * NUM_SPECIES in manifold model does not refer to the number of chemical
+  species. The commented code need to be adapted for manifold models. Not
+  attempting to address this now. SprayUnits SPU;
+
+      Vector<Real> fuelEnth(NUM_SPECIES);
+      auto eos = pele::physics::PhysicsType::eos(eosparms_h);
+      amrex::GpuArray<amrex::Real, NUM_SPECIES> mw;
+      eos.molecular_weight(mw.data());
+      m_sprayData->liqprops.init_mw(mw, m_sprayData->indx.data());
+      eos.T2Hi(m_sprayData->liqprops.ref_T, fuelEnth.data());
+      for (int ns = 0; ns < SPRAY_FUEL_NUM; ++ns) {
+        const int fspec = m_sprayData->indx[ns];
+        m_sprayData->liqprops.latentRef_minus_gasRefH_i[ns] =
+          m_sprayData->liqprops.latent[ns] - fuelEnth[fspec] *
+  SprayUnits::eng_conv;
+      }
+      for (int dir = 0; dir < AMREX_SPACEDIM; ++dir) {
+        m_sprayData->body_force[dir] = body_force[dir];
+      }*/
+
 #endif
-
-  SprayUnits SPU;
-
-  Vector<Real> fuelEnth(NUM_SPECIES);
-  auto eos = pele::physics::PhysicsType::eos(eosparms_h);
-  amrex::GpuArray<amrex::Real, NUM_SPECIES> mw;
-  eos.molecular_weight(mw.data());
-  m_sprayData->liqprops.init_mw(mw, m_sprayData->indx.data());
-  eos.T2Hi(m_sprayData->liqprops.ref_T, fuelEnth.data());
-  for (int ns = 0; ns < SPRAY_FUEL_NUM; ++ns) {
-    const int fspec = m_sprayData->indx[ns];
-    m_sprayData->liqprops.latentRef_minus_gasRefH_i[ns] =
-      m_sprayData->liqprops.latent[ns] - fuelEnth[fspec] * SprayUnits::eng_conv;
-  }
-  for (int dir = 0; dir < AMREX_SPACEDIM; ++dir) {
-    m_sprayData->body_force[dir] = body_force[dir];
-  }
 
   m_sprayData->eosparm = eosparms_d;
   Gpu::copy(Gpu::hostToDevice, m_sprayData, m_sprayData + 1, d_sprayData);
