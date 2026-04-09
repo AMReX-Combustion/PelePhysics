dbc13766ac19e5a6fa68e3af27a598ce39790ff8
d-montgomery <David.Montgomery@nrel.gov>
2026-04-09 15:31:18 -0600

    V_slab for spray_density

--- Source/Spray/SprayDerive.cpp
+++ Source/Spray/SprayDerive.cpp
@@ -18,7 +18,11 @@ SprayParticleContainer::computeDerivedVars(
   const RealVect dxi(AMREX_D_DECL(dxiarr[0], dxiarr[1], dxiarr[2]));
   const RealVect dx(AMREX_D_DECL(dxarr[0], dxarr[1], dxarr[2]));
   const RealVect plo(AMREX_D_DECL(ploarr[0], ploarr[1], ploarr[2]));
-  const Real cell_vol = AMREX_D_TERM(dx[0], *dx[1], *dx[2]);
+  Real cell_vol = AMREX_D_TERM(dx[0], *dx[1], *dx[2]);
+#if AMREX_SPACEDIM == 2
+  // Spray model in 2D assumes narrow domain in z-direction (Lz = dx)
+  cell_vol *= dx[0];
+#endif
 #ifdef AMREX_USE_EB
   const auto& factory =
     dynamic_cast<EBFArrayBoxFactory const&>(mf_var.Factory());
